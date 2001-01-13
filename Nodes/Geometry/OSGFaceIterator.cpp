e23b0d8c74da39af950c4038a9802c5a665e798a
dirk <dirk>
2001-01-13 02:02:51 +0000

    Iterator for faces, i.e. tris and quads

---
+++ Nodes/Geometry/OSGFaceIterator.cpp
@@ -0,0 +1,414 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *                 Copyright (C) 2000 by the OpenSG Forum                    *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+//---------------------------------------------------------------------------
+//  Includes
+//---------------------------------------------------------------------------
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "OSGConfig.h"
+
+#ifdef OSG_STREAM_IN_STD_NAMESPACE
+#include <iostream>
+#else
+#include <iostream.h>
+#endif
+
+#include "OSGFaceIterator.h"
+
+OSG_USING_NAMESPACE
+
+/** \enum OSGVecBase::VectorSizeE
+ *  \brief 
+ */
+
+/** \var OSGVecBase::VectorSizeE OSGVecBase::_iSize
+ * 
+ */
+
+/** \fn const char *OSGVecBase::getClassname(void)
+ *  \brief Classname
+ */
+
+/** \var OSGValueTypeT OSGVecBase::_values[iSize];
+ *  \brief Value store
+ */
+
+/***************************************************************************\
+ *                               Types                                     *
+\***************************************************************************/
+
+/***************************************************************************\
+ *                           Class variables                               *
+\***************************************************************************/
+
+char FaceIterator::cvsid[] = "@(#)$Id: OSGFaceIterator.cpp,v 1.1 2001/01/13 02:02:51 dirk Exp $";
+
+/***************************************************************************\
+ *                           Class methods                                 *
+\***************************************************************************/
+
+
+
+/*-------------------------------------------------------------------------*\
+ -  public                                                                 -
+\*-------------------------------------------------------------------------*/
+
+
+/*-------------------------------------------------------------------------*\
+ -  protected                                                              -
+\*-------------------------------------------------------------------------*/
+
+
+/*-------------------------------------------------------------------------*\
+ -  private                                                                -
+\*-------------------------------------------------------------------------*/
+
+
+
+/***************************************************************************\
+ *                           Instance methods                              *
+\***************************************************************************/
+
+/*-------------------------------------------------------------------------*\
+ -  public                                                                 -
+\*-------------------------------------------------------------------------*/
+
+/*------------- constructors & destructors --------------------------------*/
+
+/** \brief Constructor
+ */
+
+FaceIterator::FaceIterator(void) : 
+	_primIt(), _geo(),
+	_faceIndex(0), _actPrimIndex(), _facePntIndex()
+{
+}
+
+FaceIterator::FaceIterator( const GeometryPtr& geo ) :
+	_primIt(), _geo( geo ),
+	_faceIndex(0), _actPrimIndex(), _facePntIndex()
+{
+	_primIt.setGeo( geo );
+}
+
+FaceIterator::FaceIterator( const NodePtr& geo ) :
+	_primIt(), _geo( geo->getCore().dcast<GeometryPtr>() ),
+	_faceIndex(0), _actPrimIndex(), _facePntIndex()
+{
+	_primIt.setGeo( geo );
+}
+
+
+FaceIterator::FaceIterator(const FaceIterator &source) :
+	_primIt( source._primIt ), _geo( source._geo ),
+	_faceIndex(source._faceIndex), _facePntIndex()
+{
+	_facePntIndex[0] = source._facePntIndex[0];
+	_facePntIndex[1] = source._facePntIndex[1];
+	_facePntIndex[2] = source._facePntIndex[2];
+	_facePntIndex[3] = source._facePntIndex[3];
+}
+
+/** \brief Destructor
+ */
+
+FaceIterator::~FaceIterator(void)
+{
+}
+
+/*------------------------------ access -----------------------------------*/
+
+/*---------------------------- properties ---------------------------------*/
+
+/*-------------------------- your_category---------------------------------*/
+
+/** \brief increment
+ */
+
+void FaceIterator::operator++ ()
+{
+	// already at end?
+	if ( _primIt.isAtEnd() )
+	{
+		return;
+	}
+	
+	++_faceIndex;
+
+	// at end of primitive?
+	if ( _actPrimIndex >= _primIt.getPrimLength() )
+	{
+		++_primIt;
+		
+		if ( ! _primIt.isAtEnd() )
+			startPrim();
+		
+		return;
+	}
+
+	switch ( _primIt.getPrimType() )
+	{
+	case GL_TRIANGLES:  	_facePntIndex[0] = _actPrimIndex++;
+							_facePntIndex[1] = _actPrimIndex++;
+							_facePntIndex[2] = _actPrimIndex++;
+							_facePntIndex[3] = -1;
+							_actPrimIndex += 3;	
+							break;
+	case GL_QUAD_STRIP: 	_facePntIndex[0] = _facePntIndex[2];
+							_facePntIndex[1] = _facePntIndex[3];
+							_facePntIndex[3] = _actPrimIndex++;
+							_facePntIndex[2] = _actPrimIndex++;
+							break;
+	case GL_TRIANGLE_STRIP: if ( _actPrimIndex & 1 )
+							{
+								_facePntIndex[0] = _facePntIndex[2];
+							}
+							else
+							{
+								_facePntIndex[1] = _facePntIndex[2];
+							}							
+							_facePntIndex[2] = _actPrimIndex++;
+							break;
+	case GL_POLYGON:
+	case GL_TRIANGLE_FAN:	_facePntIndex[1] = _facePntIndex[2];
+							_facePntIndex[2] = _actPrimIndex++;
+							break;
+	case GL_QUADS:			_facePntIndex[0] = _actPrimIndex++;
+							_facePntIndex[1] = _actPrimIndex++;
+							_facePntIndex[2] = _actPrimIndex++;
+							_facePntIndex[3] = _actPrimIndex++;
+							break;
+	default:			SWARNING << "FaceIterator::++: encountered " 
+								  << "unknown primitive type " 
+								  << _primIt.getPrimType()
+								  << ", ignoring!" << endl;
+						if ( ! _primIt.isAtEnd() )
+							startPrim();
+						break;
+	}			
+}
+
+void FaceIterator::startPrim( void )
+{
+	_facePntIndex[0] = 0;
+	_facePntIndex[1] = 1;
+	_facePntIndex[2] = 2;
+	_facePntIndex[3] = -1;
+	_actPrimIndex = 3;
+	
+	switch ( _primIt.getPrimType() )
+	{
+	case GL_POINTS: 	// non-polygon types: ignored
+	case GL_LINES:
+	case GL_LINE_STRIP: 
+	case GL_LINE_LOOP:	++_primIt;
+						if ( ! _primIt.isAtEnd() )
+							startPrim();
+						break;
+	case GL_TRIANGLES: 
+	case GL_TRIANGLE_STRIP:
+	case GL_TRIANGLE_FAN:	if ( _primIt.getPrimLength() < 3 )
+						{
+							++_primIt;
+							if ( ! _primIt.isAtEnd() )
+								startPrim();
+							break;
+						}
+						break;
+	case GL_POLYGON:	switch ( _primIt.getPrimLength() )
+						{
+						case 0: case 1: case 2:
+							++_primIt;
+							if ( ! _primIt.isAtEnd() )
+								startPrim();
+							break;
+						case 4:
+							_facePntIndex[3] = _actPrimIndex++;	
+							break;
+						default:
+							break;
+						}
+						break;
+	case GL_QUADS:		if ( _primIt.getPrimLength() < 4 )
+						{
+							++_primIt;
+							if ( ! _primIt.isAtEnd() )
+								startPrim();
+							break;
+						}
+						_facePntIndex[3] = _actPrimIndex++;						
+						break;
+	case GL_QUAD_STRIP:	if ( _primIt.getPrimLength() < 4 )
+						{
+							++_primIt;
+							if ( ! _primIt.isAtEnd() )
+								startPrim();
+							break;
+						}
+						_facePntIndex[3] = _facePntIndex[3];						
+						_facePntIndex[2] = _actPrimIndex++;						
+						break;
+	default:			SWARNING << "FaceIterator::startPrim: encountered " 
+								  << "unknown primitive type " 
+								  << _primIt.getPrimType()
+								  << ", ignoring!" << endl;
+						++_primIt;
+						if ( ! _primIt.isAtEnd() )
+							startPrim();
+						break;
+	}			
+}
+
+void FaceIterator::setToBegin( void )
+{
+	_primIt.setToBegin();
+	_faceIndex = 0;
+	startPrim();
+}
+
+void FaceIterator::setToEnd( void )
+{
+	_primIt.setToEnd();
+	_actPrimIndex = 0;
+}
+
+/*-------------------------- assignment -----------------------------------*/
+
+/** \brief assignment
+ */
+
+FaceIterator& FaceIterator::operator = (const FaceIterator &source)
+{
+	if (this == &source)
+		return *this;
+
+	// free mem alloced by members of 'this'
+
+	// alloc new mem for members
+
+	// copy 
+	
+	this->_geo 				= source._geo;
+	this->_primIt 			= source._primIt;
+	this->_faceIndex  		= source._faceIndex;
+	this->_actPrimIndex  	= source._actPrimIndex;
+	this->_facePntIndex[0] 	= source._facePntIndex[0];
+	this->_facePntIndex[1] 	= source._facePntIndex[1];
+	this->_facePntIndex[2] 	= source._facePntIndex[2];
+	this->_facePntIndex[3] 	= source._facePntIndex[3];
+
+	return *this;
+}
+
+/*-------------------------- comparison -----------------------------------*/
+
+/** \brief assignment
+ */
+
+Bool FaceIterator::operator < (const FaceIterator &other) const
+{
+    return _primIt < other._primIt ||
+			( _primIt == other._primIt &&
+		      _actPrimIndex < other._actPrimIndex );
+}
+
+/** \brief equal
+ */
+
+Bool FaceIterator::operator == (const FaceIterator &other) const
+{
+	if ( _primIt.isAtEnd() && other._primIt.isAtEnd() )
+		return true;
+	if ( _primIt.isAtEnd() || other._primIt.isAtEnd() )
+		return false;
+    return _primIt == other._primIt &&
+			_actPrimIndex == other._actPrimIndex;
+}
+
+/** \brief unequal
+ */
+
+Bool FaceIterator::operator != (const FaceIterator &other) const
+{
+	return ! (*this == other);
+}
+
+
+/*-------------------------------------------------------------------------*\
+ -  protected                                                              -
+\*-------------------------------------------------------------------------*/
+
+
+/*-------------------------------------------------------------------------*\
+ -  private                                                                -
+\*-------------------------------------------------------------------------*/
+
+
+
+///---------------------------------------------------------------------------
+///  FUNCTION: 
+///---------------------------------------------------------------------------
+//:  Example for the head comment of a function
+///---------------------------------------------------------------------------
+///
+//p: Paramaters: 
+//p: 
+///
+//g: GlobalVars:
+//g: 
+///
+//r: Return:
+//r: 
+///
+//c: Caution:
+//c: 
+///
+//a: Assumptions:
+//a: 
+///
+//d: Description:
+//d: 
+///
+//s: SeeAlso:
+//s: 
+///---------------------------------------------------------------------------
+
---
+++ Nodes/Geometry/OSGFaceIterator.h
@@ -0,0 +1,253 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *                 Copyright (C) 2000 by the OpenSG Forum                    *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+
+#ifndef _OSGFACEITERATOR_H_
+#define _OSGFACEITERATOR_H_
+#ifdef __sgi
+#pragma once
+#endif
+
+//---------------------------------------------------------------------------
+//  Includes
+//---------------------------------------------------------------------------
+
+#include <OSGBaseTypes.h>
+#include <OSGFieldContainerPtr.h>
+#include <OSGNodeCore.h>
+#include <OSGGeometryBase.h>
+#include <OSGGeoProperty.h>
+#include <OSGPrimitiveIterator.h>
+
+OSG_BEGIN_NAMESPACE
+
+//---------------------------------------------------------------------------
+//  Forward References
+//---------------------------------------------------------------------------
+
+class Geometry;
+typedef FCPtr<NodeCorePtr, Geometry> GeometryPtr;
+
+//---------------------------------------------------------------------------
+//   Types
+//---------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------
+//  Class
+//---------------------------------------------------------------------------
+
+/*! \ingroup baselib
+ *  \brief Brief
+ *
+ *  detailed
+ */
+
+class OSG_GEOMETRY_DLLMAPPING FaceIterator
+{
+  public:
+
+    //-----------------------------------------------------------------------
+    //   constants                                                           
+    //-----------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------
+    //   enums                                                               
+    //-----------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------
+    //   types                                                               
+    //-----------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------
+    //   class functions                                                     
+    //-----------------------------------------------------------------------
+
+    static const char *getClassname(void) { return "FaceIterator"; }
+ 
+    //-----------------------------------------------------------------------
+    //   instance functions                                                  
+    //-----------------------------------------------------------------------
+
+    FaceIterator( void );
+ 
+    FaceIterator(const FaceIterator &source);
+	
+    FaceIterator( const GeometryPtr& geo );
+    FaceIterator( const NodePtr& geo );
+ 
+    virtual ~FaceIterator(void); 
+
+    /*------------------------- access -------------------------------*/
+
+	// get the triangle index
+	inline Int32    	getFaceIndex	( void 	      ) const;
+
+	// get the size of the face
+	inline Int32    	getFaceSize		( void 	      ) const;
+
+	// get the data indices/values. Indices < 0 indicate data not present
+	// in that case the value will be Nullxxx
+
+	inline Int32    	getPositionIndex( Int32 which ) const;
+	inline Pnt3f		getPosition		( Int32 which ) const;
+
+	inline Int32    	getNormalIndex	( Int32 which ) const;
+	inline Vec3f   		getNormal	 	( Int32 which ) const;
+
+	inline Int32    	getColorIndex 	( Int32 which ) const;
+	inline Color3f		getColor		( Int32 which ) const;
+
+
+    /*------------------------- your_operators ------------------------------*/
+
+    void operator ++( void );
+
+
+    /*------------------------- assignment ----------------------------------*/
+
+    FaceIterator & operator =(const FaceIterator &source);
+
+    /*------------------------- comparison ----------------------------------*/
+
+    Bool operator < (const FaceIterator &other) const;
+    
+	Bool operator == (const FaceIterator &other) const;
+	Bool operator != (const FaceIterator &other) const;
+
+  protected:
+
+    //-----------------------------------------------------------------------
+    //   enums                                                               
+    //-----------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------
+    //   types                                                               
+    //-----------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------
+    //   class variables                                                     
+    //-----------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------
+    //   class functions                                                     
+    //-----------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------
+    //   instance variables                                                  
+    //-----------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------
+    //   instance functions                                                  
+    //-----------------------------------------------------------------------
+
+	void setToBegin( void );
+	void setToEnd( void );
+	
+  private:
+
+    //-----------------------------------------------------------------------
+    //   enums                                                               
+    //-----------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------
+    //   types                                                               
+    //-----------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------
+    //   friend classes                                                      
+    //-----------------------------------------------------------------------
+
+	friend class Geometry;
+	
+    //-----------------------------------------------------------------------
+    //   friend functions                                                    
+    //-----------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------
+    //   class variables                                                     
+    //-----------------------------------------------------------------------
+
+	static char cvsid[];
+
+    //-----------------------------------------------------------------------
+    //   class functions                                                     
+    //-----------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------
+    //   instance variables                                                  
+    //-----------------------------------------------------------------------
+	
+	// the primitive Iterator it's based on
+	PrimitiveIterator _primIt;
+	
+	// the geometry (for faster access)
+	GeometryPtr 	_geo;
+	
+	// Mainly for statistics.
+	Int32 			_faceIndex;
+	
+	// index of the first actual point within the active primitive
+	// this is the index into the primitive
+	UInt32 			_actPrimIndex;
+	
+	// indices of the actual triangels' points
+	// these are indices into the primtive's indices, not actual indices
+	Int32 			_facePntIndex[4];
+	
+
+    //-----------------------------------------------------------------------
+    //   instance functions                                                  
+    //-----------------------------------------------------------------------
+
+	// called when stepping to next primitive, and at beginning
+	void startPrim( void );
+};
+
+//---------------------------------------------------------------------------
+//   Exported Types
+//---------------------------------------------------------------------------
+
+// class pointer
+
+typedef FaceIterator *FaceIteratorP;
+
+OSG_END_NAMESPACE
+
+#include "OSGFaceIterator.inl"
+
+#endif /* _OSGTRIANGLEITERATOR_H_ */
