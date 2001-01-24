fcbe5a043eca42b84bacb036efa86235e0d054c9
dirk <dirk>
2001-01-24 19:27:40 +0000

    added Colors

--- Nodes/Geometry/testGeometryRender.cpp
+++ Nodes/Geometry/testGeometryRender.cpp
@@ -123,6 +123,20 @@ int main (int argc, char **argv)
 	osgEndEditCP(pnts);
 
 
+	GeoColor4ub::PtrType cols = GeoColor4ub::create();
+	g1->setColors( cols );
+	g1->setColorPerVertex( true );
+	osgBeginEditCP(cols);
+	cols->getFieldPtr()->addValue( Color4ub( 255, 255, 255, 255) );
+	cols->getFieldPtr()->addValue( Color4ub( 255, 255, 255, 255) );
+	cols->getFieldPtr()->addValue( Color4ub(   0, 255, 255, 255) );
+	cols->getFieldPtr()->addValue( Color4ub(   0, 255, 255, 255) );
+	cols->getFieldPtr()->addValue( Color4ub( 255,   0, 255, 255) );
+	cols->getFieldPtr()->addValue( Color4ub( 255, 255,   0, 255) );
+	cols->getFieldPtr()->addValue( Color4ub( 255,   0,   0, 255) );
+	cols->getFieldPtr()->addValue( Color4ub(   0, 255,   0, 255) );
+	osgEndEditCP(cols);
+
 	GeoIndexUI32Ptr index = GeoIndexUI32::create();	
 	g1->setIndex( index );
 	osgBeginEditCP(index);
