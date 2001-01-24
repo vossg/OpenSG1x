cd922eb0da89c7c3f4fd5bd35ef18e740b144f2d
dirk <dirk>
2001-01-24 16:24:02 +0000

    IntersectAction added

---
+++ CommonPackages/commonIntersectAction.mk
@@ -0,0 +1,24 @@
+
+SUB_DIR                  := IntersectAction
+PACKAGENAME              := IntersectAction
+
+INCL_DIR                 := $(OSGPOOL)/Action/$(SUB_DIR)
+
+PROJLIBS_FILE_$(SUB_DIR) := \
+	$(OSGPOOL)/Action/$(SUB_DIR)/lib$(PACKAGENAME).$(OS).$(LIBEXT)
+
+ifeq ($(OS_BASE), NT)
+REQUIRED_SYSTEM_LIBS$(OS) := $(LIBCIO)
+else
+REQUIRED_SYSTEM_LIBS$(OS) := $(LIBCIO) -lm
+endif
+
+ifdef OSG_MK_DEBUG_RSL
+$(warning commonLog : |$(REQUIRED_SYSTEM_LIBS$(OS))|)
+endif
+
+include $(OSGPOOL)/$(OSGCOMMON)/commonPackage.mk
+
+ifdef OSG_MK_DEBUG_RSL
+$(warning commonLog : |$(POST_LINK_LIBS$(OS))|)
+endif
