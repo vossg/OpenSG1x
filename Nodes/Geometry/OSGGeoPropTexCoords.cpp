f6add323f5b789c28a061e378e7ba3b4d59481b1
dirk <dirk>
2001-02-12 01:45:24 +0000

    added TextureCoords Property

---
+++ Nodes/Geometry/OSGGeoPropTexCoords.cpp
@@ -0,0 +1,76 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *                 Copyright (C) 2000 by the OpenSG Forum                    *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+//---------------------------------------------------------------------------
+//  Includes
+//---------------------------------------------------------------------------
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "OSGConfig.h"
+
+#ifdef OSG_STREAM_IN_STD_NAMESPACE
+#include <iostream>
+#else
+#include <iostream.h>
+#endif
+
+#define OSG_COMPILEGEOMETRY
+#define OSG_COMPILEGEOPROPTEXCOORDSINST
+
+#include "OSGGeoPropTexCoords.h"
+
+OSG_BEGIN_NAMESPACE
+
+#if defined(__sgi)
+
+#pragma instantiate GeoProperty        <GeoTexCoords2fPropertyDesc>::_type
+#pragma instantiate GeoProperty        <GeoTexCoords2fPropertyDesc>::_desc
+
+#else
+
+OSG_FC_DLLEXPORT_DEF      (GeoProperty,
+                           GeoTexCoords2fPropertyDesc, 
+                           OSG_GEOMETRY_DLLTMPLMAPPING)
+
+
+#endif
+
+OSG_END_NAMESPACE
---
+++ Nodes/Geometry/OSGGeoPropTexCoords.h
@@ -0,0 +1,112 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *                 Copyright (C) 2000 by the OpenSG Forum                    *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+#ifndef _OSGGEOPROPTEXCOORDS_H_
+#define _OSGGEOPROPTEXCOORDS_H_
+#ifdef __sgi
+#pragma once
+#endif
+
+//---------------------------------------------------------------------------
+//  Includes
+//---------------------------------------------------------------------------
+
+#include <OSGGeoPropertyBase.h>
+
+OSG_BEGIN_NAMESPACE
+
+//---------------------------------------------------------------------------
+//  Forward References
+//---------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------
+//   Types
+//---------------------------------------------------------------------------
+
+
+//---------------------------------------------------------------------------
+//   Specialized Types
+//---------------------------------------------------------------------------
+
+// TexCoords 2f
+
+struct GeoTexCoords2fPropertyDesc
+{
+	static const Char8 *getTypeName (void) { return "GeoTexCoords2f";         }
+	static const Char8 *getClassName(void) { return "GeoTexCoords2fProperty"; }
+	static const Char8 *getFieldName(void) { return "TexCoords";             }
+	static const Char8 *getGroupName(void) { return "GeoTexCoords";           }
+
+	static InitContainerF getInitMethod(void) { return NULL; }
+
+	static UInt32 getFormat    (void)  { return GL_FLOAT;        }
+	static UInt32 getFormatSize(void)  { return sizeof(GLfloat); }
+	static UInt32 getDimension (void)  { return 2;               }
+	static UInt32 getStride    (void)  { return 0;               }
+
+	typedef GeoTexCoords                Inherit;
+	typedef GeoTexCoords::PtrType		InheritPtr;
+    typedef GeoTexCoordsPropertyDesc	InheritDesc;
+	typedef MFVec2f                		FieldType;
+
+    typedef InheritDesc::GenericType 	GenericType;
+};
+
+typedef GeoProperty<GeoTexCoords2fPropertyDesc> GeoTexCoords2f;
+
+#ifndef OSG_COMPILEGEOPROPTEXCOORDSINST
+#if defined(__sgi)
+
+#pragma do_not_instantiate GeoProperty<GeoTexCoords2fPropertyDesc>::_type
+#pragma do_not_instantiate GeoProperty<GeoTexCoords2fPropertyDesc>::_desc
+
+#else
+
+OSG_FC_DLLEXPORT_DECL(GeoProperty, 
+                      GeoTexCoords2fPropertyDesc, 
+                      OSG_GEOMETRY_DLLTMPLMAPPING)
+
+#endif
+#endif
+
+
+OSG_END_NAMESPACE
+
+#include <OSGGeoPropTexCoords.inl>
+
+#endif /* _OSGGEOPROPTEXCOORDS_H_ */
