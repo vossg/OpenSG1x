75ea554b0b2b2d9762ed65a89ca08cf974497806
dirk <dirk>
2001-02-12 01:44:49 +0000

    added Image FieldTypes

---
+++ Field/OSGMFImageTypes.h
@@ -0,0 +1,85 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *                 Copyright (C) 2000 by the OpenSG Forum                    *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+#ifndef _OSGMFIMAGETYPES_H_
+#define _OSGMFIMAGETYPES_H_
+#ifdef __sgi
+#pragma once
+#endif
+
+//---------------------------------------------------------------------------
+//  Includes
+//---------------------------------------------------------------------------
+
+#include <OSGMField.h>
+#include <OSGImage.h>
+#include <OSGImageFieldDataType.h>
+
+/** \file OSGMFImageTypes.h
+ *  \ingroup ImageLib
+ *  \ingroup MultiFields
+ *  \brief OpenSG image multi fields
+ * 
+ *  <UL>
+ *  <LI> osg::MFImagePointer
+ *  </UL>
+ */
+
+OSG_BEGIN_NAMESPACE
+
+/** \brief MFImagePointer
+ */
+
+typedef MField<ImageP> MFImageP;
+
+#ifndef OSG_COMPILEFIELDINST
+#if defined(__sgi)
+
+#pragma do_not_instantiate MField<ImageP>::_fieldType
+
+#else
+
+OSG_DLLEXPORT_DECL1(MField, MFImageP, OSG_IMAGE_DLLTMPLMAPPING)
+
+#endif
+#endif
+
+OSG_END_NAMESPACE
+
+#endif /* _OSGMFIMAGETYPE_H_ */
+
---
+++ Field/OSGSFImageTypes.h
@@ -0,0 +1,85 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *                 Copyright (C) 2000 by the OpenSG Forum                    *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+#ifndef _OSGSFIMAGETYPES_H_
+#define _OSGSFIMAGETYPES_H_
+#ifdef __sgi
+#pragma once
+#endif
+
+//---------------------------------------------------------------------------
+//  Includes
+//---------------------------------------------------------------------------
+
+#include <OSGSField.h>
+#include <OSGImage.h>
+#include <OSGImageFieldDataType.h>
+
+/** \file OSGSFImageTypes.h
+ *  \ingroup FieldLib
+ *  \ingroup SingleFields
+ *  \brief OpenSG image single fields
+ * 
+ *  <UL>
+ *  <LI> osg::SFImagePointer
+ *  </UL>
+ */
+
+OSG_BEGIN_NAMESPACE
+
+/** \brief SFImagePointer
+ */
+
+typedef SField<ImageP> SFImageP;
+
+#ifndef OSG_COMPILEFIELDINST
+#if defined(__sgi)
+
+#pragma do_not_instantiate SField<ImageP>::_fieldType
+
+#else
+
+OSG_DLLEXPORT_DECL1(SField, SFImageP, OSG_IMAGE_DLLTMPLMAPPING)
+
+#endif
+#endif
+
+OSG_END_NAMESPACE
+
+#endif /* _OSGSFIMAGETYPE_H_ */
+
