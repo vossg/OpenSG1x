344003732acc8b3e477a5790f25c7f998563be14
vossg <vossg>
2001-02-14 11:30:18 +0000

    *** empty log message ***

---
+++ Experimental/VRMLLoader/testVRML.cpp
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+
+#include <OSGVRMLFile.h>
+
+void main(int argc, char **argv)
+{
+    OSG::NodePtr pRoot;
+
+    int i = 0;
+
+    OSG::osgInit(i, NULL);
+
+    OSG::VRMLFile *pFile = new OSG::VRMLFile;
+
+    OSG::Node::create();
+
+    pFile->scanStandardPrototypes("std.wrl", 0);
+
+    pFile->scanFile(argv[1], 0);
+
+    pRoot = pFile->getRoot();
+
+    cerr << "Tree : " << endl;
+
+    if(pRoot == OSG::NullNode)
+    {
+        cerr << "\t Empty" << endl;
+    }
+    else
+    {
+        pRoot->dump();
+    }
+
+//    char *szFilename = "stirnwand.wrl";
+
+    OSG::osgExit();
+}
