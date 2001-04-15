1887fcc66dfdcf83326ab1d1f6ac87a95b730df3
dirk <dirk>
2001-04-15 01:44:56 +0000

    little test program for WriteFCD

---
+++ FieldContainer/testWriteFCD.cpp
@@ -0,0 +1,46 @@
+#include <OSGConfig.h>
+
+#ifdef OSG_STREAM_IN_STD_NAMESPACE
+#include <iostream>
+#include <fstream>
+#else
+#include <iostream.h>
+#include <fstream.h>
+#endif
+
+#include <OSGBaseFunctions.h>
+#include <OSGFieldContainerFactory.h>
+
+int main (int argc, char **argv)
+{
+    OSG::osgInit(argc, argv);
+
+	ostream * out = &cout;
+	char * name = "TextureChunk";
+	ofstream f;
+	
+	switch ( argc )
+	{
+	case 2: if ( ! strcmp( argv[1], "-" ) )
+				out = &cout;
+			else
+			{
+				f.open( argv[1] );
+				out = &f;
+			}
+			break;
+	case 3: if ( ! strcmp( argv[1], "-" ) )
+				out = &cout;
+			else
+			{
+				f.open( argv[1] );
+				out = &f;
+			}
+			name = argv[2];
+			break;
+	}
+	
+	OSG::FieldContainerFactory::the()->writeFCD( name, out );
+
+    OSG::osgExit();
+}
