561fe3c18a47198d15e929899ad1881490d73a0d
jbehr <jbehr>
2001-06-12 15:45:17 +0000

    generic volume draw function

---
+++ Nodes/Geometry/OSGVolumeDraw.h
@@ -0,0 +1,78 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *             Copyright (C) 2000,2001 by the OpenSG Forum                   *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+
+#ifndef _OSGVOLUMEDRAW_H_
+#define _OSGVOLUMEDRAW_H_
+#ifdef __sgi
+#pragma once
+#endif
+
+//---------------------------------------------------------------------------
+//  Includes
+//---------------------------------------------------------------------------
+
+#include <OSGConfig.h>
+#include <OSGSystemDef.h>
+#include <OSGBaseTypes.h>
+#include <OSGNode.h>
+
+#include <OSGVolume.h>
+#include <OSGBoxVolume.h>
+
+OSG_BEGIN_NAMESPACE
+
+//---------------------------------------------------------------------------
+//  Functions
+//---------------------------------------------------------------------------
+
+
+/*! \brief draw volume 
+ *  \ingroup Geometry
+ */
+OSG_SYSTEMLIB_DLLMAPPING 
+void drawVolume ( Volume &volume );
+
+/*! \brief draw box volume 
+ *  \ingroup Geometry
+ */
+OSG_SYSTEMLIB_DLLMAPPING 
+void drawVolume ( BoxVolume &volume );
+
+
+OSG_END_NAMESPACE
+
+#endif /* _OSGVOLUMEDRAW_H_ */
