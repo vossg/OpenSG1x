ee2564df7eb3268193844a3f0456195c3668632b
mroth <mroth>
2001-08-02 09:47:02 +0000

    Baseclass for all OpenSG exceptions

---
+++ Base/OSGException.cpp
@@ -0,0 +1,171 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *             Copyright (C) 2000,2001 by the OpenSG Forum                   *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+//---------------------------------------------------------------------------
+//  Includes
+//---------------------------------------------------------------------------
+
+#define OSG_COMPILEBASELIB
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "OSGConfig.h"
+#include "OSGException.h"
+
+OSG_USING_NAMESPACE
+
+/***************************************************************************\
+ *                               Types                                     *
+\***************************************************************************/
+
+/***************************************************************************\
+ *                           Class variables                               *
+\***************************************************************************/
+
+char Exception::cvsid[] = "@(#)$Id:$";
+
+/***************************************************************************\
+ *                           Class methods                                 *
+\***************************************************************************/
+
+
+
+/*-------------------------------------------------------------------------*\
+ -  public                                                                 -
+\*-------------------------------------------------------------------------*/
+
+
+/*-------------------------------------------------------------------------*\
+ -  protected                                                              -
+\*-------------------------------------------------------------------------*/
+
+
+/*-------------------------------------------------------------------------*\
+ -  private                                                                -
+\*-------------------------------------------------------------------------*/
+
+
+
+/***************************************************************************\
+ *                           Instance methods                              *
+\***************************************************************************/
+
+/*-------------------------------------------------------------------------*\
+ -  public                                                                 -
+\*-------------------------------------------------------------------------*/
+
+/*------------- constructors & destructors --------------------------------*/
+
+/** \brief Constructor
+ */
+
+Exception::Exception(void) :
+	Inherited(),
+    _what()
+{
+}
+
+
+Exception::Exception(const Exception &source) :
+	Inherited(source),
+    _what(source._what)
+{
+}
+
+/** \brief Destructor
+ */
+
+Exception::~Exception(void)
+{
+}
+
+/*------------------------------ access -----------------------------------*/
+
+/*---------------------------- properties ---------------------------------*/
+
+/*-------------------------- your_category---------------------------------*/
+
+const char *Exception::what() const
+{
+    return _what.c_str();
+}
+
+/*-------------------------- assignment -----------------------------------*/
+
+/** \brief assignment
+ */
+
+Exception& Exception::operator = (const Exception &source)
+{
+	if (this == &source)
+		return *this;
+
+	// copy parts inherited from parent
+	*(static_cast<Inherited *>(this)) = source;
+
+	// free mem alloced by members of 'this'
+
+	// alloc new mem for members
+
+	// copy 
+    _what=source._what;
+    return *this;
+}
+
+/*-------------------------- comparison -----------------------------------*/
+
+/*-------------------------------------------------------------------------*\
+ -  protected                                                              -
+\*-------------------------------------------------------------------------*/
+
+
+/*-------------------------------------------------------------------------*\
+ -  private                                                                -
+\*-------------------------------------------------------------------------*/
+
+
+OSG_BEGIN_NAMESPACE
+
+OSG_BASE_DLLMAPPING ostream &operator <<(ostream &os, const Exception &obj)
+{
+	return os << obj.what() << endl;
+} 
+
+OSG_END_NAMESPACE
+
