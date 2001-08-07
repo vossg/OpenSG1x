f7a8004cef6b9457917772cd625e1b0b809f0bb2
mroth <mroth>
2001-08-07 15:13:55 +0000

    copyToBin, copyFromBin for SFImage, MFImage added

--- Image/OSGImageFieldDataType.h
+++ Image/OSGImageFieldDataType.h
@@ -40,6 +40,7 @@
 #define _OSG_IMAGEFIELDDATATYPE_H_
 
 #include "OSGFieldDataType.h"
+#include "OSGImageFileHandler.h"
 
 OSG_BEGIN_NAMESPACE
 
@@ -76,6 +77,113 @@ struct FieldDataTraits<ImageP> : public FieldTraitsRecurseBase<ImageP>
         // TO_BE_DONE
     }
 
+    static UInt32 getBinSize(const ImageP &oObject)
+    {
+        ImageFileType *type;
+
+        type = ImageFileHandler::the().getFileType("MTD");
+        if(oObject)
+        {
+            return type->maxBufferSize(*oObject) + 
+                   sizeof(UInt64);
+        }
+        return sizeof(UInt64);
+    }
+
+    static UInt32 getBinSize(const ImageP   *pObjectStore,
+                                   UInt32    uiNumObjects)
+    {
+        UInt32 size = 0;
+
+        // defaut: individual field sizes
+        for(UInt32 i=0; i <uiNumObjects; ++i)
+        {
+            size += getBinSize(pObjectStore, uiNumObjects);
+        }
+
+        return size;
+    }
+
+    static MemoryHandle copyToBin(      MemoryHandle  pMem, 
+                                  const ImageP   &oObject)
+    {
+        UInt64 imgSize=0;
+        ImageFileType *type;
+
+        type = ImageFileHandler::the().getFileType("MTD");
+        cout << "image copyToBin: " << endl;
+        if(oObject)
+        {
+            imgSize=type->maxBufferSize(*oObject);
+        }
+        cout << "Image size " << imgSize << endl;
+        memcpy(pMem,&imgSize,sizeof(imgSize));
+        pMem+=sizeof(imgSize);
+        if(oObject)
+        {
+            type->store(*oObject,pMem);
+        }
+        return pMem + imgSize;
+    }
+
+    static MemoryHandle copyToBin(      MemoryHandle pMem, 
+                                  const ImageP      *pObjectStore,
+                                        UInt32       uiNumObjects)
+    {
+        cout << "image mcopyToBin: " << endl;
+
+        // defaut: copy each element
+        for(UInt32 i=0; i < uiNumObjects; ++i)
+        {
+            pMem = copyToBin(pMem, pObjectStore[i]);
+        }
+
+        return pMem;
+    }
+
+    static MemoryHandle copyFromBin(const MemoryHandle  pMem, 
+                                          ImageP       &oObject)
+    {
+        ImageFileType *type;
+        MemoryHandle mem=pMem;
+        UInt64 imgSize=0;
+
+        type = ImageFileHandler::the().getFileType("MTD");
+        if(oObject)
+        {
+            delete oObject;
+            oObject=NULL;
+        }
+        memcpy(&imgSize,mem,sizeof(imgSize));
+        mem+=sizeof(imgSize);
+        if(imgSize)
+        {
+            cout << "Image size " << imgSize << endl;
+            oObject=new Image();
+            type->restore(*oObject,mem);
+        }
+        ImageFileHandler::the().write(*oObject,"xxx.jpg");
+
+        mem+=imgSize;
+        cout << "image copyFromBin: " << endl;
+        return mem;
+    }
+
+    static MemoryHandle copyFromBin(const MemoryHandle  pMem, 
+                                          ImageP       *pObjectStore,
+                                          UInt32        uiNumObjects)
+    {
+        MemoryHandle mem = pMem;
+
+        // defaut: copy each element
+        for(UInt32 i = 0; i < uiNumObjects; ++i)
+        {
+            mem = copyFromBin(mem, pObjectStore[i]);
+        }
+        cout << "image mcopyFromBin: " << endl;
+
+        return mem;
+    }
 
 };
 
