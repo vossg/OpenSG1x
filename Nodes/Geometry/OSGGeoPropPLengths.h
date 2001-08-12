50d1cf338cd1cda9c820d39205bd30f727ff8646
dirk <dirk>
2001-08-12 17:13:13 +0000

    made PTypes and PLengths abstract, added concrete cases

---
+++ Nodes/Geometry/OSGGeoPropPLengths.h
@@ -0,0 +1,112 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *             Copyright (C) 2000,2001 by the OpenSG Forum                   *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+
+#ifndef _OSGGEOPROPPLENGTHS_H_
+#define _OSGGEOPROPPLENGTHS_H_
+#ifdef __sgi
+#pragma once
+#endif
+
+//---------------------------------------------------------------------------
+//  Includes
+//---------------------------------------------------------------------------
+
+#include <OSGGeoPropertyBase.h>
+
+OSG_BEGIN_NAMESPACE
+
+//---------------------------------------------------------------------------
+//  Forward References
+//---------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------
+//   Types
+//---------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------
+//   Specialized Types
+//---------------------------------------------------------------------------
+
+// PLength uint32
+
+struct GeoPLengthUI32PropertyDesc
+{
+	static const Char8 *getTypeName (void) { return "GeoPLengthUI32";         }
+	static const Char8 *getClassName(void) { return "GeoPLengthUI32Property"; }
+	static const Char8 *getFieldName(void) { return "Indices";              }
+	static const Char8 *getGroupName(void) { return "GeoPLength";             }
+
+	static InitContainerF getInitMethod(void) { return NULL; }
+
+	static UInt32 getFormat    (void) { return GL_UNSIGNED_INT; }
+	static UInt32 getFormatSize(void) { return sizeof(GLuint);  }
+	static UInt32 getDimension (void) { return 1;               }
+	static UInt32 getStride    (void) { return 0;               }
+
+	typedef GeoPLength                 Inherit;
+	typedef GeoPLength::PtrType        InheritPtr;
+    typedef GeoPLengthPropertyDesc     InheritDesc;
+	typedef MFUInt32                 FieldType;
+
+    typedef InheritDesc::GenericType GenericType;
+ 
+    typedef GeoPropertyInterface<GeoPLengthPropertyDesc> Interface;
+};
+
+typedef GeoProperty<GeoPLengthUI32PropertyDesc> GeoPLengthUI32;
+
+
+#ifndef OSG_COMPILEGEOPROPPLENGTHINST
+#if defined(__sgi)
+
+#pragma do_not_instantiate GeoProperty<GeoPLengthUI32PropertyDesc>::_type
+#pragma do_not_instantiate GeoProperty<GeoPLengthUI32PropertyDesc>::_desc
+
+#else
+
+OSG_FC_DLLEXPORT_DECL(GeoProperty, 
+                      GeoPLengthUI32PropertyDesc, 
+                      OSG_SYSTEMLIB_DLLTMPLMAPPING)
+
+#endif
+#endif
+
+OSG_END_NAMESPACE
+
+#endif /* _OSGGEOPROPPLENGTHS_H_ */
---
+++ Nodes/Geometry/OSGGeoPropPTypes.h
@@ -0,0 +1,112 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *             Copyright (C) 2000,2001 by the OpenSG Forum                   *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+
+#ifndef _OSGGEOPROPPTYPES_H_
+#define _OSGGEOPROPPTYPES_H_
+#ifdef __sgi
+#pragma once
+#endif
+
+//---------------------------------------------------------------------------
+//  Includes
+//---------------------------------------------------------------------------
+
+#include <OSGGeoPropertyBase.h>
+
+OSG_BEGIN_NAMESPACE
+
+//---------------------------------------------------------------------------
+//  Forward References
+//---------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------
+//   Types
+//---------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------
+//   Specialized Types
+//---------------------------------------------------------------------------
+
+// PType uint8
+
+struct GeoPTypeUI8PropertyDesc
+{
+	static const Char8 *getTypeName (void) { return "GeoPTypeUI8";         }
+	static const Char8 *getClassName(void) { return "GeoPTypeUI8Property"; }
+	static const Char8 *getFieldName(void) { return "Types";               }
+	static const Char8 *getGroupName(void) { return "GeoPType";            }
+
+	static InitContainerF getInitMethod(void) { return NULL; }
+
+	static UInt32 getFormat    (void) { return GL_UNSIGNED_BYTE; }
+	static UInt32 getFormatSize(void) { return sizeof(GLubyte);  }
+	static UInt32 getDimension (void) { return 1;                }
+	static UInt32 getStride    (void) { return 0;                }
+
+	typedef GeoPType                 Inherit;
+	typedef GeoPType::PtrType        InheritPtr;
+    typedef GeoPTypePropertyDesc     InheritDesc;
+	typedef MFUInt8                  FieldType;
+
+    typedef InheritDesc::GenericType GenericType;
+ 
+    typedef GeoPropertyInterface<GeoPTypePropertyDesc> Interface;
+};
+
+typedef GeoProperty<GeoPTypeUI8PropertyDesc> GeoPTypeUI8;
+
+
+#ifndef OSG_COMPILEGEOPROPPTYPEINST
+#if defined(__sgi)
+
+#pragma do_not_instantiate GeoProperty<GeoPTypeUI8PropertyDesc>::_type
+#pragma do_not_instantiate GeoProperty<GeoPTypeUI8PropertyDesc>::_desc
+
+#else
+
+OSG_FC_DLLEXPORT_DECL(GeoProperty, 
+                      GeoPTypeUI8PropertyDesc, 
+                      OSG_SYSTEMLIB_DLLTMPLMAPPING)
+
+#endif
+#endif
+
+OSG_END_NAMESPACE
+
+#endif /* _OSGGEOPROPPTYPES_H_ */
