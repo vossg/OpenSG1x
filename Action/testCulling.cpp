d11f938f552285b0d3e6e72c35eb19cc4baef3ab
dirk <dirk>
2001-09-30 20:12:16 +0000

    test for culling

---
+++ Action/testCulling.cpp
@@ -0,0 +1,405 @@
+#include <OSGConfig.h>
+
+#ifdef OSG_STREAM_IN_STD_NAMESPACE
+#include <iostream>
+#else
+#include <iostream.h>
+#endif
+
+#include <GL/glut.h>
+
+#include <OSGFieldContainerFactory.h>
+#include <OSGSFSysTypes.h>
+#include <OSGVector.h>
+#include <OSGQuaternion.h>
+#include <OSGMatrix.h>
+#include <OSGMatrixUtility.h>
+#include <OSGBoxVolume.h>
+#include <OSGNode.h>
+#include <OSGGroup.h>
+#include <OSGThread.h>
+#include <OSGTransform.h>
+#include <OSGAttachment.h>
+#include <OSGMFVecTypes.h>
+#include <OSGAction.h>
+#include <OSGDrawAction.h>
+#include <OSGSimpleGeometry.h>
+#include <OSGSceneFileHandler.h>
+
+#include <OSGDirectionalLight.h>
+#include <OSGVRMLTransform.h>
+#include <OSGSimpleMaterial.h>
+
+#include "OSGViewport.h"
+#include "OSGCamera.h"
+#include "OSGWindow.h"
+#include "OSGGLUTWindow.h"
+#include "OSGCamera.h"
+#include "OSGPerspectiveCamera.h"
+#include "OSGSolidBackground.h"
+
+#include "OSGTrackball.h"
+
+OSG_USING_NAMESPACE
+
+
+DrawAction * ract;
+
+NodePtr  root;
+
+NodePtr  georoot;
+
+PerspectiveCameraPtr cam;
+ViewportPtr vp;
+WindowPtr win;
+
+TransformPtr cam_trans;
+
+Trackball tball;
+
+int mouseb = 0;
+int lastx=0, lasty=0;
+
+void 
+display(void)
+{
+	Matrix m1, m2, m3;
+    Quaternion q1;
+
+    tball.getRotation().getValue(m3);
+
+    q1.setValue(m3);
+
+    m1.setRotate(q1);   
+	m2.setTranslate( tball.getPosition() );
+	
+	m1.mult( m2 );
+    
+    // MatrixLookAt( m1, 0,0,5, 0,0,0, 0,1,0 );
+	cam_trans->getSFMatrix()->setValue( m1 );
+
+	win->draw( ract );
+}
+
+void reshape( int w, int h )
+{
+	cerr << "Reshape: " << w << "," << h << endl;
+	win->resize( w, h );
+}
+
+
+void
+animate(void)
+{
+	glutPostRedisplay();
+}
+
+// tball stuff
+
+
+void
+motion(int x, int y)
+{	
+	Real32 w = win->getWidth(), h = win->getHeight();
+	
+
+	Real32	a = -2. * ( lastx / w - .5 ),
+				b = -2. * ( .5 - lasty / h ),
+				c = -2. * ( x / w - .5 ),
+				d = -2. * ( .5 - y / h );
+
+	if ( mouseb & ( 1 << GLUT_LEFT_BUTTON ) )
+	{
+		tball.updateRotation( a, b, c, d );	
+	}
+	else if ( mouseb & ( 1 << GLUT_MIDDLE_BUTTON ) )
+	{
+		tball.updatePosition( a, b, c, d );	
+	}
+	else if ( mouseb & ( 1 << GLUT_RIGHT_BUTTON ) )
+	{
+		tball.updatePositionNeg( a, b, c, d );	
+	}
+	lastx = x;
+	lasty = y;
+}
+
+void
+mouse(int button, int state, int x, int y)
+{
+	if ( state == 0 )
+	{
+		switch ( button )
+		{
+		case GLUT_LEFT_BUTTON:	break;
+		case GLUT_MIDDLE_BUTTON:tball.setAutoPosition(true);
+								break;
+		case GLUT_RIGHT_BUTTON:	tball.setAutoPositionNeg(true);
+								break;
+		}
+		mouseb |= 1 << button;
+	}
+	else if ( state == 1 )
+	{
+		switch ( button )
+		{
+		case GLUT_LEFT_BUTTON:	break;
+		case GLUT_MIDDLE_BUTTON:tball.setAutoPosition(false);
+								break;
+		case GLUT_RIGHT_BUTTON:	tball.setAutoPositionNeg(false);
+								break;
+		}		
+		mouseb &= ~(1 << button);
+	}
+	lastx = x;
+	lasty = y;
+}
+
+void
+vis(int visible)
+{
+	if (visible == GLUT_VISIBLE) 
+	{
+		glutIdleFunc(animate);
+	} 
+	else 
+	{
+		glutIdleFunc(NULL);
+	}
+}
+
+void key(unsigned char key, int x, int y)
+{
+	switch ( key )
+	{
+        case 27:	osgExit(); exit(0);
+        case 'a':	glDisable( GL_LIGHTING );
+            cerr << "Lighting disabled." << endl;
+            break;
+        case 's':	glEnable( GL_LIGHTING );
+            cerr << "Lighting enabled." << endl;
+            break;
+        case 'z':	glPolygonMode( GL_FRONT_AND_BACK, GL_POINT);
+            cerr << "PolygonMode: Point." << endl;
+            break;
+        case 'x':	glPolygonMode( GL_FRONT_AND_BACK, GL_LINE);
+            cerr << "PolygonMode: Line." << endl;
+            break;
+        case 'c':	glPolygonMode( GL_FRONT_AND_BACK, GL_FILL);
+            cerr << "PolygonMode: Fill." << endl;
+            break;
+        case 'r':	cerr << "Sending ray through " << x << "," << y << endl;
+            {
+	    Line l;
+            cam->calcViewRay( l, x, y, *vp );
+            cerr << "From " << l.getPosition() << ", dir " << l.getDirection() << endl;
+            }
+	    break;
+        case 'd':	ract->setAutoFrustum( ! ract->getAutoFrustum() );
+            cerr << "autoFrustum: " << (ract->getAutoFrustum()?"true":"false")
+	         << endl;
+            break;
+        case 'f':	ract->setVolumeDrawing( ! ract->getVolumeDrawing() );
+            cerr << "VolumeDrawing: " << (ract->getVolumeDrawing()?"true":"false")
+	         << endl;
+            break;
+	}
+}
+
+
+int main (int argc, char **argv)
+{
+	osgInit(argc,argv);
+
+	// GLUT init
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode( GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+	int winid = glutCreateWindow("OpenSG");
+	glutKeyboardFunc(key);
+	glutVisibilityFunc(vis);
+	glutReshapeFunc(reshape);
+	glutDisplayFunc(display);       
+	glutMouseFunc(mouse);   
+	glutMotionFunc(motion); 
+	
+	glutIdleFunc(display);	
+	
+	glEnable( GL_DEPTH_TEST );
+	glEnable( GL_LIGHTING );
+
+	// create the graph
+
+	// beacon for camera and light	
+    NodePtr b1n = Node::create();
+    GroupPtr b1 = Group::create();
+	beginEditCP(b1n);
+	b1n->setCore( b1 );
+	endEditCP(b1n);
+
+	// transformation
+    NodePtr t1n = Node::create();
+    TransformPtr t1 = Transform::create();
+	beginEditCP(t1n);
+	t1n->setCore( t1 );
+	t1n->addChild( b1n );
+	endEditCP(t1n);
+
+	cam_trans = t1;
+
+	// light
+	
+	NodePtr dlight = Node::create();
+	DirectionalLightPtr dl = DirectionalLight::create();
+
+	beginEditCP(dlight);
+	dlight->setCore( dl );
+	endEditCP(dlight);
+	
+	beginEditCP(dl);
+	dl->setAmbient( .3, .3, .3, 1 );
+	dl->setDiffuse( 1, 1, 1, 1 );
+	dl->setSpecular( 1, 1, 1, 1 );
+	dl->setDirection(0,0,1);
+	dl->setBeacon( b1n);
+	endEditCP(dl);
+	
+	// root
+    root = Node::create();
+    GroupPtr gr1 = Group::create();
+	beginEditCP(root);
+	root->setCore( gr1 );
+	root->addChild( t1n );
+	root->addChild( dlight );
+	endEditCP(root);
+
+
+	// Create the test geometry
+    
+    if ( argc >= 2 )
+    {
+        georoot = SceneFileHandler::the().read(argv[1], 0);
+    }
+    else
+    {
+        georoot = Node::create();
+        gr1 = Group::create();
+	    beginEditCP(georoot);
+	    georoot->setCore( gr1 );
+	    endEditCP(georoot);
+
+	    SimpleMaterialPtr mat = SimpleMaterial::create();
+	    beginEditCP(mat);
+	    mat->setDiffuse( Color3f( 1,0,0 ) );
+	    mat->setAmbient( Color3f( 1,0,0 ) );
+	    mat->setSpecular( Color3f( 1,1,1 ) );
+	    mat->setShininess( 10 );
+	    endEditCP(mat);
+
+	    GeometryPtr sphere = makeSphereGeo( 2, 1 );
+	    beginEditCP(sphere);
+	    sphere->setMaterial( mat );
+	    endEditCP(sphere);
+
+	    // create a bunch of instances, moved by transforms
+
+	    beginEditCP(georoot);
+
+	    for ( int x = -10; x < 10; x += 2)
+	    {
+		    // reuse the geometry
+		    NodePtr gn = Node::create();		
+		    beginEditCP(gn);
+		    gn->setCore( sphere );
+		    endEditCP(gn);
+
+		    // the transform to move it
+		    NodePtr tn = Node::create();
+		    VRMLTransformPtr t = VRMLTransform::create();
+
+		    beginEditCP(tn);
+		    tn->setCore( t );
+		    tn->addChild( gn );
+		    endEditCP(tn);
+
+		    beginEditCP(t);
+		    t->setTranslation( Vec3f( x, 0, 0 ) );
+		    endEditCP(t);
+
+		    // add it to the georoot
+
+		    georoot->addChild( tn );
+	    }
+
+	    endEditCP(georoot);
+	}
+    
+
+	Vec3f min,max;
+	georoot->getVolume().getBounds( min, max );
+	
+	cout << "Volume: from " << min << " to " << max << endl;
+
+	beginEditCP(dlight);
+	dlight->addChild( georoot );
+	endEditCP(dlight);
+
+	// Camera
+	
+	cam = PerspectiveCamera::create();
+	cam->setBeacon( b1n );
+	cam->setFov( deg2rad( 90. ) );
+	cam->setNear( 0.1 );
+	cam->setFar( 10000 );
+
+	// Background
+	SolidBackgroundPtr bkgnd = SolidBackground::create();
+
+	// Viewport
+
+	vp = Viewport::create();
+	vp->setCamera( cam );
+	vp->setBackground( bkgnd );
+	vp->setRoot( root );
+	vp->setSize( 0,0, 1,1 );
+
+	// Window
+	cout << "GLUT winid: " << winid << endl;
+
+	GLUTWindowPtr gwin;
+
+	GLint glvp[4];
+	glGetIntegerv( GL_VIEWPORT, glvp );
+
+	gwin = GLUTWindow::create();
+	gwin->setWinID(winid);
+	gwin->setSize( glvp[2], glvp[3] );
+
+	win = gwin;
+
+	win->addPort( vp );
+
+	win->init();
+
+	// Action
+	
+	ract = DrawAction::create();
+
+    	ract->setAutoFrustum( true );
+	
+	// tball
+
+	Vec3f pos( 0, 0, 5 );
+
+	tball.setMode( Trackball::OSGObject );
+	tball.setStartPosition( pos, true );
+	tball.setSum( true );
+	tball.setTranslationMode( Trackball::OSGFree );
+
+	// run...
+	
+	glutMainLoop();
+	
+    return 0;
+}
+
