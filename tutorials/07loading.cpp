29a3dee6fe9bbe2cd70149efc403b5b29a1a6194
dirk <dirk>
2001-09-30 20:10:09 +0000

    made makefile a little more flexible added examples for loading and paralle loading

---
+++ tutorials/07loading.cpp
@@ -0,0 +1,90 @@
+
+// Minimalistic OpenSG program
+
+// GLUT is used for window handling
+#include <GL/glut.h>
+
+#include <OpenSG/OSGConfig.h>
+#include <OpenSG/OSGLog.h>
+#include <OpenSG/OSGGLUTWindow.h>
+#include <OpenSG/OSGSimpleSceneManager.h>
+#include <OpenSG/OSGSimpleGeometry.h>
+
+#include <OpenSG/OSGSceneFileHandler.h>
+
+OSG_USING_NAMESPACE
+
+SimpleSceneManager *mgr;
+
+// Standard GLUT callback functions
+void display( void )
+{
+    mgr->redraw();
+}
+
+void reshape( int w, int h )
+{
+    mgr->resize( w, h );
+    glutPostRedisplay();
+}
+
+void
+motion(int x, int y)
+{
+    mgr->mouseMove( x, y );
+    glutPostRedisplay();
+}
+
+void
+mouse(int button, int state, int x, int y)
+{
+    if ( state )
+        mgr->mouseButtonRelease( button, x, y );
+    else
+        mgr->mouseButtonPress( button, x, y );
+    glutPostRedisplay();
+}
+
+// Initialize GLUT & OpenSG and set up the scene
+int main (int argc, char **argv)
+{
+    // OSG init
+    osgInit(argc,argv);
+
+    // GLUT init
+    glutInit(&argc, argv);
+    glutInitDisplayMode( GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+    int winid = glutCreateWindow("OpenSG");
+    glutReshapeFunc(reshape);
+    glutDisplayFunc(display);
+    glutMouseFunc(mouse);
+    glutMotionFunc(motion);
+
+    // the connection between GLUT and OpenSG
+    GLUTWindowPtr gwin= GLUTWindow::create();
+    gwin->setWinID(winid);
+    gwin->init();
+
+    // load the scene
+
+    if ( argc < 2 )
+    {
+        FFATAL(( "No file given!\n" ));
+        return 1;
+    }
+    
+    NodePtr scene = SceneFileHandler::the().read(argv[1]);
+
+    // create the SimpleSceneManager helper
+    mgr = new SimpleSceneManager;
+
+    mgr->setWindow( gwin );
+    mgr->setRoot( scene );
+
+    mgr->showAll();
+
+    // GLUT main loop
+    glutMainLoop();
+
+    return 0;
+}
