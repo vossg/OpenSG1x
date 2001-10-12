f1bd650b89ca6288d570794f703743984d12a6f7
jbehr <jbehr>
2001-10-12 10:44:19 +0000

    OSGQGLManagedWidget and test (testManagedWindowQT_qt.cpp) added

---
+++ Window/QT/testManagedWindowQT_qt.cpp
@@ -0,0 +1,40 @@
+#include <qapplication.h>
+
+#include <OSGConfig.h>
+#include <OSGSceneFileHandler.h>
+#include <OSGQGLManagedWidget_qt.h>
+
+int main( int argc, char ** argv )
+{
+  osg::osgInit(argc,argv);
+
+  int retCode = -1;
+  QApplication qapp( argc, argv );
+  osg::OSGQGLManagedWidget widget;
+  osg::NodePtr root;
+  
+
+  if (argc > 1)
+    {
+      root = osg::SceneFileHandler::the().read(argv[1]);
+      if (root == osg::NullFC)
+        {
+          FINFO (( "Couldn't load %s", argv[1]));
+        }
+      else
+        {
+          widget.getManager().setRoot(root);
+          widget.getManager().showAll();
+          widget.show();
+          retCode = qapp.exec();
+        }
+    }
+  else
+    {
+      FINFO (( "%s usage: $s 'sceneFileName'\n", argv[0], argv[0] ));
+    }
+  
+  osg::osgExit();
+
+  return retCode;
+}
