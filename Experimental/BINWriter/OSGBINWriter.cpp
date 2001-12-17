ba80cbcf4ca97e7bf9b3e9f84ed1d2f3f88bb732
mroth <mroth>
2001-12-17 12:34:14 +0000

    Binary writer

---
+++ Experimental/BINWriter/OSGBINWriter.cpp
@@ -0,0 +1,344 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *             Copyright (C) 2000,2001 by the OpenSG Forum                   *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+//---------------------------------------------------------------------------
+//  Includes
+//---------------------------------------------------------------------------
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <string>
+#include <map>
+#include <vector>
+#include <fstream>
+
+#include <OSGNode.h>
+#include <OSGBaseFunctions.h>
+#include <OSGFieldContainer.h>
+#include <OSGSFFieldContainerPtr.h>
+#include <OSGMFFieldContainerPtr.h>
+#include <OSGIDString.h>
+#include <OSGBinaryDataHandler.h>
+
+#include <OSGConfig.h>
+#include "OSGBINWriter.h"
+
+OSG_USING_NAMESPACE
+
+#ifdef __sgi
+#pragma set woff 1174
+#endif
+
+namespace
+{
+    static Char8 cvsid_cpp[] = "@(#)$Id: $";
+    static Char8 cvsid_hpp[] = OSG_HEADER_CVSID;
+}
+
+#ifdef __sgi
+#pragma reset woff 1174
+#endif
+
+/*! \class osg::OSGBINWriter
+    OSGBINWriter documentation,
+ */
+
+
+/*-------------------------------------------------------------------------*/
+/*                            Constructors                                 */
+
+
+BINWriter::BINWriter(FILE *file) :
+           _outFileHandler(file)
+{
+}
+
+BINWriter::FCInfo::FCInfo() :
+        ptr (NullFC),
+        type(osg::IDString()),
+        mask(FieldBits::NoField)
+{
+}
+ 
+
+
+BINWriter::BinaryFileHandler::BinaryFileHandler(FILE *file)
+		: OSG::BinaryDataHandler(0), _file(file)
+{
+    _readMemory .resize(10000);
+    _writeMemory.resize(10000);
+    readBufAdd  (&_readMemory [0], _readMemory.size());
+    writeBufAdd (&_writeMemory[0], _writeMemory.size());
+}
+
+/*-------------------------------------------------------------------------*/
+/*                             Destructor                                  */
+
+/*! Destructor documentation
+ */
+BINWriter::~BINWriter(void)
+{
+}
+
+BINWriter::BinaryFileHandler::~BinaryFileHandler(void)
+{
+}
+
+void BINWriter::addToIdMap(FieldContainerPtr fcPtr)
+{
+    if (fcPtr==NullFC)
+	{
+		cerr<<"ERROR in BINWriter::addToIdMap : NullFC"<<endl;
+		return;
+	}
+	FCTypeIdMap::iterator iIdMap;
+
+	//add new ContainerType
+	if (_fcIdMap.find(fcPtr->getType().getCName())==_fcIdMap.end())
+	{
+		vector<UInt32> idVec;
+		idVec.push_back(0);
+		if (!(_fcIdMap.insert(pair<std::string, vector<UInt32> >
+                        (fcPtr->getType().getCName(), idVec)).second))
+        {
+                cerr<<"BINWriter::addToIdMap(FieldContainerPtr):"<<endl
+                    <<"ERROR while inserting into _fcIdMap"<<endl;
+        };
+	}
+	
+	//add container to the vector of appropriate type
+    iIdMap = _fcIdMap.find(fcPtr->getType().getCName());
+	iIdMap->second.push_back(fcPtr.getFieldContainerId());
+
+	//increase the 0th entry of the vector (number of entries)
+	iIdMap->second[0]++;
+}
+
+/*-------------------------------------------------------------------------*/
+/*Write a file header containing the following:                            */
+/*-------------------------------------------------------------------------*/
+/* Number (k) of roots (osg::UInt32)                                       */
+/*   {                                                                     */
+/*       root IDs (osg::UInt32)                                            */
+/*   } (k times)                                                           */
+/* Number(i) of different containertypes (osg::UInt32)                     */
+/*   {                                                                     */
+/*   containertype (std::string)                                           */
+/*   number(j) of appropriate containers (osg::UInt32)                     */
+/*     {                                                                   */
+/*     container Ids (osg::UInt32)                                         */
+/*     } (j times)                                                         */
+/*                                                                         */
+/*   } (i times)                                                           */
+/*-------------------------------------------------------------------------*/
+
+void BINWriter::writeFileHeader()
+{
+	FCTypeIdMap    ::iterator iIdMap = _fcIdMap.begin();
+	vector<NodePtr>::iterator iRoots = _vec_pRootNodes.begin();
+    UInt32 mapSize    = _fcIdMap.size();
+	UInt32 numOfRoots = _vec_pRootNodes.size();
+    UInt32 i = 0, j = 0;
+    
+    _outFileHandler.putUInt32(numOfRoots);
+    for(i=0; i!=numOfRoots; ++i, ++iRoots)
+    {
+       _outFileHandler.putUInt32(iRoots->getFieldContainerId());
+    }
+
+    _outFileHandler.putUInt32(mapSize);
+   	for(i=0; i<mapSize ; ++i, ++iIdMap)
+	{
+		_outFileHandler.putString(iIdMap->first);
+		for (j=0; &(iIdMap->second[j])!=iIdMap->second.end(); ++j)
+		{
+			_outFileHandler.putUInt32(iIdMap->second[j]);
+		}
+	}
+}
+
+
+void BINWriter::BinaryFileHandler
+		::read(OSG::MemoryHandle mem, OSG::UInt32 size)
+{
+    fread(mem, size, 1, _file);
+}
+
+void BINWriter::BinaryFileHandler
+		::write(OSG::MemoryHandle mem, OSG::UInt32 size)
+{
+    fwrite(mem, size, 1, _file);
+}
+
+
+void BINWriter::write(NodePtr node)
+{
+	_fcMap.clear();
+	_fcIdMap.clear();
+	_vec_pRootNodes.clear();
+    _vec_pRootNodes.push_back(node);
+	//traverse and index containers to write
+	doIndexFC(node);
+	//write the header with number and type of containers
+	writeFileHeader();
+	//write the containers and flush the filehandler
+	doWriteIndexedFC();
+	_outFileHandler.flush();
+}
+
+void BINWriter::write(vector<NodePtr> nodes)
+{
+    //does the same as write(NodePtr) for every Node in the vector
+	_fcMap.clear();
+	_fcIdMap.clear();
+    _vec_pRootNodes.clear();
+    _vec_pRootNodes = nodes;
+
+    vector<NodePtr>::const_iterator iter = nodes.begin();
+    for(; iter != nodes.end(); ++iter)
+    {
+        doIndexFC( *iter );
+    }
+	writeFileHeader();
+	doWriteIndexedFC();
+	_outFileHandler.flush();
+}
+
+void BINWriter::doIndexFC(FieldContainerPtr fieldConPtr)
+{
+    //traverse
+	if(fieldConPtr == NullFC)
+	{
+		//nothing to do
+        return;
+	}
+
+    FieldContainerType &fcType = fieldConPtr->getType();
+	UInt32 i;
+	FCInfo fcInfo;
+
+	//go through all fields
+	for(i = 1; i <= fcType.getNumFieldDescs(); ++i)
+    {
+		FieldDescription *fDesc    = fcType.getFieldDescription(i);
+        Field            *fieldPtr = fieldConPtr->getField(i);
+        const FieldType  &fType    = fieldPtr->getType();
+
+        if(!fDesc->isInternal())
+        { 
+            //detect referenced containers #DIRTY-HACK#
+			if( strstr(fType.getCName(), "Ptr") != NULL )
+            { 
+                //traverse the referenced SF/MF-containers
+				if(fieldPtr->getCardinality() == FieldType::SINGLE_FIELD)
+                { 
+					doIndexFC(
+                    ((SFFieldContainerPtr *) fieldPtr)->getValue());
+
+                }
+                else if(fieldPtr->getCardinality() == 
+                        FieldType::MULTI_FIELD       )
+                { 
+					UInt32 j;
+                    for(j = 0;
+                        j < ((MFFieldContainerPtr*)fieldPtr)->getSize();
+                        ++j)
+                    { 
+						doIndexFC(((MFFieldContainerPtr*)fieldPtr)
+								   ->getValue(j));
+                    }
+
+                }
+
+			}
+            //ignore node volume
+            if(fcType == Node::getClassType() &&
+               fcType.getFieldDescription(i)->getFieldMask() == 
+               Node::VolumeFieldMask)
+            {
+                continue;
+            }
+            // ignore empty mfields
+            if(fieldPtr->getCardinality() == FieldType::MULTI_FIELD &&
+               fieldPtr->getSize() == 0)
+            {
+                continue;
+            }
+            //add field to fieldmask
+            fcInfo.mask = (fcInfo.mask | fcType.getFieldDescription(i)
+                                                ->getFieldMask());
+        }
+
+	}
+	//add container to the Map of containers to write
+	if( _fcMap.find(fieldConPtr.getFieldContainerId()) == _fcMap.end() )
+	{
+		fcInfo.ptr  = fieldConPtr;
+		fcInfo.type = fieldConPtr->getType().getName();
+        
+		if (!(_fcMap.insert(pair<UInt32, FCInfo>
+                            (fieldConPtr.getFieldContainerId(), fcInfo)).second))
+        {
+            SWARNING<<"BINWriter::doIndexFC(FieldContainerPtr):"<<endl
+                    <<"ERROR while inserting into _fcMap"<<endl;
+        };
+		//add container id to index
+        addToIdMap(fcInfo.ptr);
+    }		
+}
+
+
+void BINWriter::doWriteIndexedFC()
+{
+	FCInfoMap::const_iterator i=_fcMap.begin();
+	UInt32 mapSize = _fcMap.size(),count = 1;
+
+    for( ; i != _fcMap.end(); ++i, ++count)
+	{
+        SINFO<<"writing container "<<setw(4)
+             <<count<<"/"<<mapSize<<"..."<<endl;
+        //for each entry in _fcMap
+	    //write ID
+        _outFileHandler.putUInt32(i->first);
+        //write FieldMask
+        _outFileHandler.putUInt32(i->second.mask);
+	    //write data
+	    i->second.ptr->copyToBin(_outFileHandler, i->second.mask);
+	}
+}
---
+++ Experimental/BINWriter/testBINWriter.cpp
@@ -0,0 +1,55 @@
+#include <OSGConfig.h>
+ 
+#ifdef OSG_STREAM_IN_STD_NAMESPACE
+#include <iostream>
+#include <fstream>
+#else
+#include <iostream.h>
+#include <fstream.h>
+#endif
+
+#include <OSGNode.h>
+#include <OSGBaseFunctions.h>
+#include <OSGSceneFileHandler.h>
+#include <OSGBINWriter.h>
+
+int main (int argc, char *argv[])
+{
+    for ( int i = 0; i < argc; i++ )
+        cout << "Param " << i << ":" << argv[i] << endl;
+
+    OSG::osgInit(argc, argv);
+
+    FILE *outFile;
+    char *inFileName  = "tie.wrl";
+    char *outFileName = "tie.bin";
+    if( argc > 1 )
+        inFileName  = argv[1];
+
+	if (argc > 2)
+			outFileName = argv[2];
+    
+    
+	if(fopen(inFileName, "rb")!=NULL)
+	{
+		outFile = fopen(outFileName, "wb");
+		
+		if (outFile==NULL)
+			cerr<<"ERROR: Cannot create file """<<outFileName<<""""<<endl;
+		else
+		{
+			OSG::NodePtr root = OSG::SceneFileHandler::the().read(inFileName,0);
+    		cout << "#starting binary conversion for "<<inFileName<<endl;
+			OSG::BINWriter writer(outFile);
+    		writer.write( root );
+			cout<<"#finished!"<<endl;
+//            root->dump();
+		}
+
+		fclose(outFile);
+	}
+	else cerr<<inFileName<<" not found!"<<endl;
+    
+	
+	return 0;
+}
