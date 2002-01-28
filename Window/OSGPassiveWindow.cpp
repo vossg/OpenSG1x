e5f44e84d5e6f35c50ea5c696a6ee94680fbf931
dirk <dirk>
2002-01-28 21:24:11 +0000

    preview check-in for Juggler developers

---
+++ Window/OSGPassiveWindow.cpp
@@ -0,0 +1,138 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *             Copyright (C) 2000,2001 by the OpenSG Forum                   *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+//---------------------------------------------------------------------------
+//  Includes
+//---------------------------------------------------------------------------
+
+#define OSG_COMPILESYSTEMLIB
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <OSGConfig.h>
+
+#include "OSGPassiveWindow.h"
+
+OSG_USING_NAMESPACE
+
+#ifdef __sgi
+#pragma set woff 1174
+#endif
+
+namespace
+{
+    static char cvsid_cpp[] = "@(#)$Id: OSGPassiveWindow.cpp,v 1.1 2002/01/28 21:24:11 dirk Exp $";
+    static char cvsid_hpp[] = OSGPASSIVEWINDOW_HEADER_CVSID;
+    static char cvsid_inl[] = OSGPASSIVEWINDOW_INLINE_CVSID;
+}
+
+#ifdef __sgi
+#pragma reset woff 1174
+#endif
+
+/*! \class osg::PassiveWindow
+
+*/
+
+/*----------------------- constructors & destructors ----------------------*/
+
+//! Constructor
+
+PassiveWindow::PassiveWindow(void) :
+    Inherited()
+{
+}
+
+//! Copy Constructor
+
+PassiveWindow::PassiveWindow(const PassiveWindow &source) :
+    Inherited(source)
+{
+}
+
+//! Destructor
+
+PassiveWindow::~PassiveWindow(void)
+{
+}
+
+/*----------------------------- class specific ----------------------------*/
+
+//! initialize the static features of the class, e.g. action callbacks
+
+void PassiveWindow::initMethod (void)
+{
+}
+
+//! react to field changes
+
+void PassiveWindow::changed(BitVector, ChangeMode)
+{
+}
+
+//! output the instance for debug purposes
+
+void PassiveWindow::dump(      UInt32    , 
+                         const BitVector ) const
+{
+    SLOG << "Dump PassiveWindow NI" << endl;
+}
+
+// init the window: create the context  
+void PassiveWindow::init( void )
+{
+    setupGL();
+}
+    
+// activate the window: bind the OGL context    
+void PassiveWindow::activate( void )
+{
+}
+    
+// swap front and back buffers  
+void PassiveWindow::swap( void )
+{
+}
+
+// Query for a GL extension function
+PassiveWindow::PassiveWindowExtFunc PassiveWindow::getFunctionByName(const Char8 *s)
+{
+    return PassiveWindowExtFunc(s);
+}
+
---
+++ Window/OSGPassiveWindow.h
@@ -0,0 +1,141 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *             Copyright (C) 2000,2001 by the OpenSG Forum                   *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+#ifndef _OSGPASSIVEWINDOW_H_
+#define _OSGPASSIVEWINDOW_H_
+#ifdef __sgi
+#pragma once
+#endif
+
+#include <OSGConfig.h>
+
+#include <OSGPassiveWindowBase.h>
+
+OSG_BEGIN_NAMESPACE
+
+/*! \brief *put brief class description here* 
+ */
+
+class OSG_SYSTEMLIB_DLLMAPPING PassiveWindow : public PassiveWindowBase
+{
+  private:
+
+    typedef PassiveWindowBase Inherited;
+
+    /*==========================  PUBLIC  =================================*/
+  public:
+    typedef void (*PassiveWindowExtFunc)(void);
+
+    /*---------------------------------------------------------------------*/
+    /*! \name                      Sync                                    */
+    /*! \{                                                                 */
+
+    virtual void changed(BitVector  whichField, 
+                         ChangeMode from);
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                     Output                                   */
+    /*! \{                                                                 */
+
+    virtual void dump(      UInt32     uiIndent = 0, 
+                      const BitVector  bvFlags  = 0) const;
+
+    /*! \}                                                                 */
+    
+    /*---------------------------------------------------------------------*/
+    /*! \name                      Redefined                               */
+    /*! \{                                                                 */
+    
+    virtual PassiveWindowExtFunc getFunctionByName(const Char8 *s);
+    
+    virtual void init( void );
+    
+    virtual void activate( void );
+    
+    virtual void deactivate ( void ) {}
+    
+    virtual void swap( void );    
+    
+    /*! \}                                                                 */    
+    /*=========================  PROTECTED  ===============================*/
+  protected:
+
+    // Variables should all be in PassiveWindowBase.
+
+    /*---------------------------------------------------------------------*/
+    /*! \name                  Constructors                                */
+    /*! \{                                                                 */
+
+    PassiveWindow(void);
+    PassiveWindow(const PassiveWindow &source);
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                   Destructors                                */
+    /*! \{                                                                 */
+
+    virtual ~PassiveWindow(void); 
+
+    /*! \}                                                                 */
+    
+    /*==========================  PRIVATE  ================================*/
+  private:
+
+    friend class FieldContainer;
+    friend class PassiveWindowBase;
+
+    static void initMethod(void);
+
+    // prohibit default functions (move to 'public' if you need one)
+
+    void operator =(const PassiveWindow &source);
+};
+
+typedef PassiveWindow *PassiveWindowP;
+
+typedef FCPtr<WindowPtr, PassiveWindow> PassiveWindowPtr;
+
+OSG_END_NAMESPACE
+
+#include <OSGPassiveWindow.inl>
+#include <OSGPassiveWindowBase.inl>
+
+#define OSGPASSIVEWINDOW_HEADER_CVSID "@(#)$Id: OSGPassiveWindow.h,v 1.1 2002/01/28 21:24:11 dirk Exp $"
+
+#endif /* _OSGPASSIVEWINDOW_H_ */
