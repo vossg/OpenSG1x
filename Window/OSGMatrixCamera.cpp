47bd725beccd00b87be5225c93e9d14d93312d51
dirk <dirk>
2002-01-29 17:50:57 +0000

    preview checkin for Juggler Support

---
+++ Window/OSGMatrixCamera.cpp
@@ -0,0 +1,184 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *             Copyright (C) 2000,2001 by the OpenSG Forum                   *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+//---------------------------------------------------------------------------
+//  Includes
+//---------------------------------------------------------------------------
+
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <OSGConfig.h>
+
+#include "OSGMatrixCamera.h"
+
+OSG_USING_NAMESPACE
+
+
+/***************************************************************************\
+ *                            Description                                  *
+\***************************************************************************/
+
+/*! \class OSG::MatrixCamera
+
+A perspective camera with a symmetric frustum.
+
+*/
+
+/***************************************************************************\
+ *                               Types                                     *
+\***************************************************************************/
+
+/***************************************************************************\
+ *                           Class variables                               *
+\***************************************************************************/
+
+char MatrixCamera::cvsid[] = "@(#)$Id: OSGMatrixCamera.cpp,v 1.1 2002/01/29 17:50:57 dirk Exp $";
+
+/***************************************************************************\
+ *                           Class methods                                 *
+\***************************************************************************/
+
+/*-------------------------------------------------------------------------*\
+ -  public                                                                 -
+\*-------------------------------------------------------------------------*/
+
+/***************************************************************************\
+ *                           Class methods                                 *
+\***************************************************************************/
+
+/*-------------------------------------------------------------------------*\
+ -  public                                                                 -
+\*-------------------------------------------------------------------------*/
+
+/*-------------------------------------------------------------------------*\
+ -  protected                                                              -
+\*-------------------------------------------------------------------------*/
+
+/*-------------------------------------------------------------------------*\
+ -  private                                                                -
+\*-------------------------------------------------------------------------*/
+
+/** \brief initialize the static features of the class, e.g. action callbacks
+ */
+
+void MatrixCamera::initMethod (void)
+{
+}
+
+/***************************************************************************\
+ *                           Instance methods                              *
+\***************************************************************************/
+
+/*-------------------------------------------------------------------------*\
+ -  public                                                                 -
+\*-------------------------------------------------------------------------*/
+
+
+/*------------- constructors & destructors --------------------------------*/
+
+/** \brief Constructor
+ */
+
+MatrixCamera::MatrixCamera(void) :
+    Inherited()
+{
+}
+
+/** \brief Copy Constructor
+ */
+
+MatrixCamera::MatrixCamera(const MatrixCamera &source) :
+    Inherited(source)
+{
+}
+
+/** \brief Destructor
+ */
+
+MatrixCamera::~MatrixCamera(void)
+{
+}
+
+
+/** \brief react to field changes
+ */
+
+void MatrixCamera::changed(BitVector, ChangeMode)
+{
+}
+
+/*------------------------------- dump ----------------------------------*/
+
+/** \brief output the instance for debug purposes
+ */
+
+void MatrixCamera::dump(      UInt32     OSG_CHECK_ARG(uiIndent), 
+                         const BitVector OSG_CHECK_ARG(bvFlags)) const
+{
+	SLOG << "Dump MatrixCamera NI" << endl;
+}
+
+void MatrixCamera::draw( DrawAction *    OSG_CHECK_ARG(action), 
+                         const Viewport& OSG_CHECK_ARG(port) )
+{
+}
+
+void MatrixCamera::getProjection( Matrix& result, UInt32 OSG_CHECK_ARG(width), 
+                                                  UInt32 OSG_CHECK_ARG(height))
+{
+	result = getProjectionMatrix();
+}
+
+void MatrixCamera::getViewing( Matrix& result, UInt32 OSG_CHECK_ARG(width), 
+                                               UInt32 OSG_CHECK_ARG(height))
+{
+	result = getModelviewMatrix();
+}
+ 
+
+/*-------------------------------------------------------------------------*\
+ -  protected                                                              -
+\*-------------------------------------------------------------------------*/
+
+/*-------------------------------------------------------------------------*\
+ -  private                                                                -
+\*-------------------------------------------------------------------------*/
+
