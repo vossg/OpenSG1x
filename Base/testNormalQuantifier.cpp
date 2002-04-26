c498847842c10970c49f31a1b54debd2e922ad7d
jbehr <jbehr>
2002-04-26 10:44:35 +0000

    *** empty log message ***

--- Base/testNormalQuantifier.cpp
+++ Base/testNormalQuantifier.cpp
@@ -1,6 +1,8 @@
 
 #include <stdio.h>
 
+#include <iostream>
+
 #include <OSGBaseTypes.h>
 #include <OSGBaseFunctions.h>
 #include <OSGLog.h>
@@ -11,13 +13,27 @@ int main (int argc, char **argv)
 {
 	int retCode = 0;
 
+	osg::UInt32 index, retIndex;
 	osg::NormalQuantifier nq;
+	osg::Vec3f vec(5,7,6);
 
 	FLOG (("Build NormalQuantifier with 4 subdivisions\n"));
 
 	nq.build(4);
 
 	FLOG (("Normal count: %d\n", nq.getNormalCount()));
+
+	index = nq.getNormalCount() / 2;
+
+	vec = nq.getNormal(index);
+
+	cout << vec << endl;
+
+	retIndex = nq.getIndex(vec);
+
+	cout << retIndex << endl;
+
+	FLOG (("Index: %d\n", retIndex ));
 
 	return retCode;
 }
