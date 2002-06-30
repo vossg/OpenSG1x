85d7c3909a414593ca053176c06b5af56a71bbdb
dirk <dirk>
2002-06-30 17:05:38 +0000

    typo fixes

--- Nodes/Geometry/OSGSimpleGeometry.cpp
+++ Nodes/Geometry/OSGSimpleGeometry.cpp
@@ -16,11 +16,11 @@
  *                                                                           *
  * This library is distributed in the hope that it will be useful, but       *
  * WITHOUT ANY WARRANTY; without even the implied warranty of                *
- * MERCHANTABILITY or FITNESS forA PARTICULAR PURPOSE.  See the GNU         *
- * Library General Public License formore details.                          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
  *                                                                           *
  * You should have received a copy of the GNU Library General Public         *
- * License along with this library; ifnot, write to the Free Software       *
+ * License along with this library; if not, write to the Free Software       *
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
  *                                                                           *
 \*---------------------------------------------------------------------------*/
