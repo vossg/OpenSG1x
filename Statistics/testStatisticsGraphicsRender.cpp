52e6880203f36f4f53ad240941b23d79141c5118
dirk <dirk>
2002-07-29 22:42:12 +0000

    fixed compile problem

--- Statistics/testStatisticsGraphicsRender.cpp
+++ Statistics/testStatisticsGraphicsRender.cpp
@@ -155,7 +155,7 @@ int main(int argc, char **argv)
                      0.0,
                      STATISTICS_INVERT | STATISTICS_OVERFLOW_RESIZE
                      | STATISTICS_SMOOTH | STATISTICS_ENABLE_POINTS
-                     | STATISTICS_MAX_TEXT | STATISTICS_MIN_TEXT
+                     | STATISTICS_MAX_TEXT | STATISTICS_MIN_TEXT,
                      100,
                      "FPS"
                      );
