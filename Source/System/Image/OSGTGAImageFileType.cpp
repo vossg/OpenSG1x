4aa43b0a1216acac767d632c3e6abea92548d55d
dirk <dirk>
2002-11-21 17:07:15 +0000

    basic TGA image loader

---
+++ Source/System/Image/OSGTGAImageFileType.cpp
@@ -0,0 +1,318 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *             Copyright (C) 2000-2002 by the OpenSG Forum                   *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+//-------------------------------
+// Includes  
+//-------------------------------
+
+#ifdef __hpux // prevent int32 clash (model.h/tiff.h)  
+#define _INT32
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "OSGConfig.h"
+#include "OSGBaseFunctions.h"
+
+#include "OSGTGAImageFileType.h"
+
+#ifdef OSG_WITH_TGA
+#include <tiffio.h>
+#endif
+#include <OSGLog.h>
+
+
+OSG_USING_NAMESPACE
+
+
+/*! \class osg::TGAImageFileType 
+    \ingroup GrpSystemImage
+    
+*/
+
+// Static Class Varible implementations:
+static const Char8 *suffixArray[] = {
+  "tga"
+};
+
+
+void TGAImageFileType::readHeader(std::istream &in, TGAHeader &header)
+{
+    UInt8 dum[18];
+    
+    in.read(dum, 18);
+
+    header.idLength      = dum[ 0];
+    header.colorMapType  = dum[ 1];
+    header.imageType     = dum[ 2];
+    header.cmapEntrySize = dum[ 7];
+    header.depth         = dum[16];
+    header.descriptor    = dum[17];
+   
+    if(osgIsBigEndian())
+    {
+        header.cmapFirst  = dum[ 4] + dum[ 3] * 256;
+        header.cmapLength = dum[ 6] + dum[ 5] * 256;
+        header.xOrigin    = dum[ 9] + dum[ 8] * 256;
+        header.yOrigin    = dum[11] + dum[10] * 256;
+        header.width      = dum[13] + dum[12] * 256;
+        header.height     = dum[15] + dum[14] * 256;
+    }
+    else
+    {
+        header.cmapFirst  = dum[ 3] + dum[ 4] * 256;
+        header.cmapLength = dum[ 5] + dum[ 6] * 256;
+        header.xOrigin    = dum[ 8] + dum[ 9] * 256;
+        header.yOrigin    = dum[10] + dum[11] * 256;
+        header.width      = dum[12] + dum[13] * 256;
+        header.height     = dum[14] + dum[15] * 256;
+    }
+}
+
+bool TGAImageFileType::readCompressedImageData(std::istream &in, Image &image, 
+                                                TGAImageFileType::TGAHeader &header)
+{
+    UInt32 npix = image.getWidth() * image.getHeight();
+    UInt8   rep;
+    UChar8 *data = image.getData();
+    UInt16 bpp = image.getBpp();
+    UChar8 r,g,b,a;
+        
+    while(npix)
+    {
+        in.read(&rep, 1);
+        
+        if(rep & 0x80)
+        {
+            rep = (rep & 0x7f) + 1;
+            npix -= rep;
+            
+            switch(image.getPixelFormat())
+            {
+            case Image::OSG_L_PF:
+                                        in.read(&a, 1);
+                                        while(rep--)
+                                        {
+                                            *data++ = a;
+                                        }
+                                        break;
+            case Image::OSG_RGB_PF:     
+                                        in.read(&b, 1);
+                                        in.read(&g, 1);
+                                        in.read(&r, 1);
+                                        while(rep--)
+                                        {
+                                            *data++ = b;
+                                            *data++ = g;
+                                            *data++ = r;
+                                        }
+                                        break;
+            case Image::OSG_RGBA_PF:
+                                        in.read(&b, 1);
+                                        in.read(&g, 1);
+                                        in.read(&r, 1);
+                                        in.read(&a, 1);
+                                        while(rep--)
+                                        {
+                                            *data++ = b;
+                                            *data++ = g;
+                                            *data++ = r;
+                                            *data++ = a;
+                                        }
+                                        break;
+            default:                    FWARNING(("TGA: unknown pixel "
+                                            "format!?!\n"));
+                                        return false;
+            }
+        }
+        else // raw packet
+        {
+            rep = (rep & 0x7f) + 1;
+            in.read(data, bpp * rep);
+            data += rep * bpp;
+            npix -= rep;
+        }   
+    }
+    return true;
+}
+
+
+TGAImageFileType TGAImageFileType::_the(suffixArray, sizeof(suffixArray));
+
+bool TGAImageFileType::read(      Image &image, 
+                            const Char8 *fileName)
+{
+    std::ifstream  in(fileName, std::ios::in | std::ios::binary);
+    
+    if(! in.rdbuf()->is_open())
+    {
+        FWARNING(("Error opening TGA file %s!\n", fileName));
+        return false;
+    }
+
+    TGAHeader header;   
+    readHeader(in, header);
+    
+    Image::PixelFormat format = Image::OSG_INVALID_PF;
+ 
+    bool    compressed = header.imageType & 0x8;
+    
+    switch(header.imageType & ~0x8)
+    {
+    case 1:     FWARNING(("TGA: 8-bit image not supported!\n"));
+                break;
+                
+    case 2:     switch(header.depth)
+                {
+                case 24:    format = Image::OSG_RGB_PF;
+                            break;
+                case 32:    format = Image::OSG_RGBA_PF;
+                            break;
+                default:    FWARNING(("TGA: Unknown pixel depth %s!\n", 
+                                    header.depth));
+                            break;
+                }
+                break;
+                
+    case 3:     format = Image::OSG_L_PF;
+                break;
+    }
+
+    if(format == Image::OSG_INVALID_PF)
+    {
+       FWARNING(("Unsupported image type for TGA file %s!\n", fileName));
+       return false; 
+    }
+    
+    image.set(format, header.width, header.height);
+
+    // read the image ID
+    UInt8 imageid[256];
+    in.read(imageid, header.idLength);
+    imageid[header.idLength] = 0;
+    
+    FDEBUG(("TGA: Image ID '%s'\n", imageid));
+    
+    // read color map data
+    if(header.colorMapType == 1)
+    {
+        UInt32 len = osgMin(header.cmapEntrySize / 3, 8) * header.cmapLength;
+        
+        UInt8 * dum = new UInt8 [len];
+        
+        in.read(dum, len);
+    
+        delete [] dum;
+    }
+    
+    // read image data
+    if(compressed)
+    {
+        if(!readCompressedImageData(in, image, header))
+        {
+            FWARNING(("Unsupported image type for TGA file %s!\n", fileName));
+            return false; 
+        }
+    }
+    else
+    {
+        in.read(image.getData(), image.getSize());
+    }
+
+    // check origin
+    switch(header.descriptor & 0x30)
+    {
+    case 0x00:  // bottom left, ok!
+                break;
+    case 0x10:  // bottom right
+    case 0x20:  // top left
+    case 0x30:  // top right
+                FWARNING(("TGA: origin 0x%d not supported!\n", 
+                          header.descriptor & 0x30));
+                return false;
+    }
+
+    // do BGR -> RGB swap, as GL_BGR_EXT is not supported everywhere
+    if(image.getPixelFormat() == Image::OSG_RGB_PF ||
+       image.getPixelFormat() == Image::OSG_RGBA_PF)
+    {
+        UChar8 *d    = image.getData(), dum;
+        UInt32  npix = image.getWidth() * image.getHeight();
+        UInt8   bpp  = image.getBpp();
+        
+        while(npix--)
+        {
+            dum  = d[2];
+            d[2] = d[0];
+            d[0] = dum;
+            d += bpp;
+        }
+    }
+   
+    return true;
+}
+
+bool TGAImageFileType::write(const Image &image,
+                             const Char8 *fileName)
+{
+    SWARNING <<
+        getMimeType() <<
+        " write is not compiled into the current binary " <<
+        std::endl;
+
+    return true;
+}
+
+
+TGAImageFileType::TGAImageFileType(const Char8 *suffixArray[],
+                                   UInt16 suffixByteCount) :
+    ImageFileType(suffixArray, suffixByteCount)
+{
+    return;
+}
+
+TGAImageFileType::TGAImageFileType(const TGAImageFileType &obj) :
+    ImageFileType(obj)
+{
+    return;
+}
+
+TGAImageFileType::~TGAImageFileType(void)
+{
+    return;
+}
---
+++ Source/System/Image/OSGTGAImageFileType.h
@@ -0,0 +1,137 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *             Copyright (C) 2000-2002 by the OpenSG Forum                   *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+#ifndef OSGTGAIMAGEFILETYPE_CLASS_DECLARATION
+#define OSGTGAIMAGEFILETYPE_CLASS_DECLARATION
+#ifdef  __sgi
+#pragma  once
+#endif
+
+#include <OSGSystemDef.h>
+#include <OSGBaseTypes.h>
+#include <OSGImageFileType.h>
+
+OSG_BEGIN_NAMESPACE
+
+
+class OSG_SYSTEMLIB_DLLMAPPING TGAImageFileType : public ImageFileType
+{
+    /*==========================  PUBLIC  =================================*/
+  public:
+
+    /*---------------------------------------------------------------------*/
+    /*! \name                   Destructor                                 */
+    /*! \{                                                                 */
+
+    virtual ~TGAImageFileType (void);
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                  Get Method                                  */
+    /*! \{                                                                 */
+
+    virtual TGAImageFileType the          (void) { return _the; }
+
+    virtual const Char8      *getMimeType (void) { return "TGA"; }
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                   Read/Write                                 */
+    /*! \{                                                                 */
+
+    virtual bool read  (Image &image, const Char8 *fileName);
+
+    virtual bool write (const Image &image, const Char8 *fileName);
+
+    /*! \}                                                                 */
+
+    /*=========================  PROTECTED  ===============================*/
+  protected:
+
+    /*---------------------------------------------------------------------*/
+    /*! \name               Default Constructor                            */
+    /*! \{                                                                 */
+
+    TGAImageFileType (const Char8 *suffixArray[], UInt16 suffixByteCount );
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                Copy Constructor                              */
+    /*! \{                                                                 */
+
+    TGAImageFileType (const TGAImageFileType &obj);
+
+    /*! \}                                                                 */
+
+    /*==========================  PRIVATE  ================================*/
+  private:
+
+    // header struct
+
+    typedef struct 
+    {
+        UInt8   idLength;
+        UInt8   colorMapType;
+        UInt8   imageType;
+        UInt16  cmapFirst;
+        UInt16  cmapLength;
+        UInt8   cmapEntrySize;
+        UInt16  xOrigin;
+        UInt16  yOrigin;
+        UInt16  width;
+        UInt16  height;
+        UInt8   depth;
+        UInt8   descriptor;
+    } 
+    TGAHeader;
+
+    void readHeader(std::istream &in, TGAHeader &header);
+
+    bool readCompressedImageData(std::istream &in, Image &image, TGAHeader &header);
+
+    typedef ImageFileType Inherited;
+
+    static TGAImageFileType _the;
+
+};
+
+typedef TGAImageFileType* TGAImageFileTypeP;
+
+OSG_END_NAMESPACE
+
+#endif // OSGTGAIMAGEFILETYPE_CLASS_DECLARATION
