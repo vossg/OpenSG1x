74eef1dcc20106e7a514f2ba7c14a19f6da6758c
vossg <vossg>
2002-11-25 03:59:06 +0000

    fixed : linux icc70 compile probs (ENDIAN defines)

--- Source/Base/Base/OSGConfig.h
+++ Source/Base/Base/OSGConfig.h
@@ -60,6 +60,12 @@
 #include <machine/endian.h>
 #elif defined(__linux)
 #include <endian.h>
+#ifndef __USE_BSD
+# define LITTLE_ENDIAN  __LITTLE_ENDIAN
+# define BIG_ENDIAN     __BIG_ENDIAN
+# define PDP_ENDIAN     __PDP_ENDIAN
+# define BYTE_ORDER     __BYTE_ORDER
+#endif
 #elif defined(WIN32) // HACK until I find a better solution
 #define LITTLE_ENDIAN 1234
 #define BIG_ENDIAN 4321
