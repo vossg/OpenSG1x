e73492ced042459012debe50a367dce04d1c5e73
a-m-z <a-m-z>
2003-08-15 09:00:33 +0000

    updated the uic generated files.

--- Examples/SceneViewer/OSGSceneView_qt.cpp
+++ Examples/SceneViewer/OSGSceneView_qt.cpp
@@ -1,50 +1,47 @@
 /****************************************************************************
 ** Form implementation generated from reading ui file 'OSGSceneView_qt.ui'
 **
-** Created: Mon Sep 2 18:52:38 2002
-**      by:  The User Interface Compiler (uic)
+** Created: Fr 15. Aug 10:56:01 2003
+**      by: The User Interface Compiler ($Id: qt/main.cpp   3.1.2   edited Dec 19 11:45 $)
 **
 ** WARNING! All changes made in this file will be lost!
 ****************************************************************************/
+
 #include "OSGSceneView_qt.h"
 
-#include <qvariant.h>   // first for gcc 2.7.2
-#include <OpenSG/OSGBINWriter.h>
-#include <OpenSG/OSGLog.h>
-#include <OpenSG/OSGMFFieldContainerPtr.h>
-#include <OpenSG/OSGNode.h>
-#include <OpenSG/OSGNodeCore.h>
-#include <OpenSG/OSGSFFieldContainerPtr.h>
-#include <OpenSG/OSGSceneFileHandler.h>
-#include <OpenSG/OSGSimpleAttachments.h>
+#include <qvariant.h>
 #include <qfiledialog.h>
+#include <OpenSG/OSGSceneFileHandler.h>
+#include <qpopupmenu.h>
+#include <OpenSG/OSGSFFieldContainerPtr.h>
+#include <OpenSG/OSGMFFieldContainerPtr.h>
+#include <OpenSG/OSGNodeCore.h>
+#include <OpenSG/OSGNode.h>
+#include <OpenSG/OSGLog.h>
+#include <OpenSG/OSGBINWriter.h>
+#include <OpenSG/OSGSimpleAttachments.h>
+#include <qpushbutton.h>
+#include <qsplitter.h>
 #include <qgroupbox.h>
 #include <qheader.h>
 #include <qlistview.h>
-#include <qpopupmenu.h>
-#include <qpushbutton.h>
-#include <qsplitter.h>
 #include <qtable.h>
-#include "OpenSG/OSGQGLManagedWidget_qt.h"
 #include <qlayout.h>
 #include <qtooltip.h>
 #include <qwhatsthis.h>
+#include "OpenSG/OSGQGLManagedWidget_qt.h"
 #include "OSGSceneView_qt.ui.h"
 
 /* 
- *  Constructs a OSGSceneView which is a child of 'parent', with the 
+ *  Constructs a OSGSceneView as a child of 'parent', with the 
  *  name 'name' and widget flags set to 'f'.
  */
-OSGSceneView::OSGSceneView( QWidget* parent,  const char* name, WFlags fl )
+OSGSceneView::OSGSceneView( QWidget* parent, const char* name, WFlags fl )
     : QWidget( parent, name, fl )
 {
     if ( !name )
 	setName( "OSGSceneView" );
-    resize( 667, 537 ); 
-    setCaption( trUtf8( "Form1", "" ) );
-    OSGSceneViewLayout = new QHBoxLayout( this ); 
-    OSGSceneViewLayout->setSpacing( 6 );
-    OSGSceneViewLayout->setMargin( 2 );
+    OSGSceneViewLayout = new QHBoxLayout( this, 2, 6, "OSGSceneViewLayout"); 
 
     mainSplitter = new QSplitter( this, "mainSplitter" );
     mainSplitter->setFrameShape( QSplitter::MShape );
@@ -55,21 +52,18 @@ OSGSceneView::OSGSceneView( QWidget* parent,  const char* name, WFlags fl )
     Splitter3->setOrientation( QSplitter::Horizontal );
 
     GroupBox1 = new QGroupBox( Splitter3, "GroupBox1" );
-    GroupBox1->setTitle( trUtf8( "Tree", "" ) );
     GroupBox1->setColumnLayout(0, Qt::Vertical );
-    GroupBox1->layout()->setSpacing( 0 );
-    GroupBox1->layout()->setMargin( 0 );
+    GroupBox1->layout()->setSpacing( 6 );
+    GroupBox1->layout()->setMargin( 2 );
     GroupBox1Layout = new QHBoxLayout( GroupBox1->layout() );
     GroupBox1Layout->setAlignment( Qt::AlignTop );
-    GroupBox1Layout->setSpacing( 6 );
-    GroupBox1Layout->setMargin( 2 );
 
     treeListView = new QListView( GroupBox1, "treeListView" );
-    treeListView->addColumn( trUtf8( "Node Type", "" ) );
-    treeListView->addColumn( trUtf8( "Core Type", "" ) );
-    treeListView->addColumn( trUtf8( "Node Name", "" ) );
-    treeListView->addColumn( trUtf8( "Core Name", "" ) );
-    treeListView->addColumn( trUtf8( "Core USEed", "" ) );
+    treeListView->addColumn( tr( "Node Type" ) );
+    treeListView->addColumn( tr( "Core Type" ) );
+    treeListView->addColumn( tr( "Node Name" ) );
+    treeListView->addColumn( tr( "Core Name" ) );
+    treeListView->addColumn( tr( "Core USEed" ) );
     treeListView->setRootIsDecorated( TRUE );
     GroupBox1Layout->addWidget( treeListView );
 
@@ -77,47 +71,56 @@ OSGSceneView::OSGSceneView( QWidget* parent,  const char* name, WFlags fl )
     Splitter5->setOrientation( QSplitter::Vertical );
 
     GroupBox3 = new QGroupBox( Splitter5, "GroupBox3" );
-    GroupBox3->setTitle( trUtf8( "Info", "" ) );
     GroupBox3->setColumnLayout(0, Qt::Vertical );
-    GroupBox3->layout()->setSpacing( 0 );
-    GroupBox3->layout()->setMargin( 0 );
+    GroupBox3->layout()->setSpacing( 6 );
+    GroupBox3->layout()->setMargin( 2 );
     GroupBox3Layout = new QHBoxLayout( GroupBox3->layout() );
     GroupBox3Layout->setAlignment( Qt::AlignTop );
-    GroupBox3Layout->setSpacing( 6 );
-    GroupBox3Layout->setMargin( 2 );
 
     infoTable = new QTable( GroupBox3, "infoTable" );
-    infoTable->setNumCols( infoTable->numCols() + 1 );    infoTable->horizontalHeader()->setLabel( infoTable->numCols() - 1, trUtf8( "Value", "" ) );
-    infoTable->setNumRows( infoTable->numRows() + 1 );    infoTable->verticalHeader()->setLabel( infoTable->numRows() - 1, trUtf8( "Node Type Name", "" ) );
-    infoTable->setNumRows( infoTable->numRows() + 1 );    infoTable->verticalHeader()->setLabel( infoTable->numRows() - 1, trUtf8( "Node Name", "" ) );
-    infoTable->setNumRows( infoTable->numRows() + 1 );    infoTable->verticalHeader()->setLabel( infoTable->numRows() - 1, trUtf8( "Core Type Name", "" ) );
-    infoTable->setNumRows( infoTable->numRows() + 1 );    infoTable->verticalHeader()->setLabel( infoTable->numRows() - 1, trUtf8( "Core Name", "" ) );
-    infoTable->setNumRows( infoTable->numRows() + 1 );    infoTable->verticalHeader()->setLabel( infoTable->numRows() - 1, trUtf8( "Core USEed count", "" ) );
+    infoTable->setNumCols( infoTable->numCols() + 1 );
+    infoTable->horizontalHeader()->setLabel( infoTable->numCols() - 1, tr( "Value" ) );
+    infoTable->setNumRows( infoTable->numRows() + 1 );
+    infoTable->verticalHeader()->setLabel( infoTable->numRows() - 1, tr( "Node Type Name" ) );
+    infoTable->setNumRows( infoTable->numRows() + 1 );
+    infoTable->verticalHeader()->setLabel( infoTable->numRows() - 1, tr( "Node Name" ) );
+    infoTable->setNumRows( infoTable->numRows() + 1 );
+    infoTable->verticalHeader()->setLabel( infoTable->numRows() - 1, tr( "Core Type Name" ) );
+    infoTable->setNumRows( infoTable->numRows() + 1 );
+    infoTable->verticalHeader()->setLabel( infoTable->numRows() - 1, tr( "Core Name" ) );
+    infoTable->setNumRows( infoTable->numRows() + 1 );
+    infoTable->verticalHeader()->setLabel( infoTable->numRows() - 1, tr( "Core USEed count" ) );
     infoTable->setNumRows( 5 );
     infoTable->setNumCols( 1 );
     GroupBox3Layout->addWidget( infoTable );
 
     GroupBox5 = new QGroupBox( Splitter5, "GroupBox5" );
-    GroupBox5->setTitle( trUtf8( "Fields", "" ) );
     GroupBox5->setColumnLayout(0, Qt::Vertical );
-    GroupBox5->layout()->setSpacing( 0 );
-    GroupBox5->layout()->setMargin( 0 );
+    GroupBox5->layout()->setSpacing( 6 );
+    GroupBox5->layout()->setMargin( 2 );
     GroupBox5Layout = new QHBoxLayout( GroupBox5->layout() );
     GroupBox5Layout->setAlignment( Qt::AlignTop );
-    GroupBox5Layout->setSpacing( 6 );
-    GroupBox5Layout->setMargin( 2 );
 
     fieldsTable = new QTable( GroupBox5, "fieldsTable" );
-    fieldsTable->setNumCols( fieldsTable->numCols() + 1 );    fieldsTable->horizontalHeader()->setLabel( fieldsTable->numCols() - 1, trUtf8( "Name", "" ) );
-    fieldsTable->setNumCols( fieldsTable->numCols() + 1 );    fieldsTable->horizontalHeader()->setLabel( fieldsTable->numCols() - 1, trUtf8( "Type", "" ) );
-    fieldsTable->setNumCols( fieldsTable->numCols() + 1 );    fieldsTable->horizontalHeader()->setLabel( fieldsTable->numCols() - 1, trUtf8( "Value", "" ) );
-    fieldsTable->setNumCols( fieldsTable->numCols() + 1 );    fieldsTable->horizontalHeader()->setLabel( fieldsTable->numCols() - 1, trUtf8( "Cardinality", "" ) );
-    fieldsTable->setNumCols( fieldsTable->numCols() + 1 );    fieldsTable->horizontalHeader()->setLabel( fieldsTable->numCols() - 1, trUtf8( "ID", "" ) );
-    fieldsTable->setNumCols( fieldsTable->numCols() + 1 );    fieldsTable->horizontalHeader()->setLabel( fieldsTable->numCols() - 1, trUtf8( "Mask", "" ) );
+    fieldsTable->setNumCols( fieldsTable->numCols() + 1 );
+    fieldsTable->horizontalHeader()->setLabel( fieldsTable->numCols() - 1, tr( "Name" ) );
+    fieldsTable->setNumCols( fieldsTable->numCols() + 1 );
+    fieldsTable->horizontalHeader()->setLabel( fieldsTable->numCols() - 1, tr( "Type" ) );
+    fieldsTable->setNumCols( fieldsTable->numCols() + 1 );
+    fieldsTable->horizontalHeader()->setLabel( fieldsTable->numCols() - 1, tr( "Value" ) );
+    fieldsTable->setNumCols( fieldsTable->numCols() + 1 );
+    fieldsTable->horizontalHeader()->setLabel( fieldsTable->numCols() - 1, tr( "Cardinality" ) );
+    fieldsTable->setNumCols( fieldsTable->numCols() + 1 );
+    fieldsTable->horizontalHeader()->setLabel( fieldsTable->numCols() - 1, tr( "ID" ) );
+    fieldsTable->setNumCols( fieldsTable->numCols() + 1 );
+    fieldsTable->horizontalHeader()->setLabel( fieldsTable->numCols() - 1, tr( "Mask" ) );
     fieldsTable->setNumRows( 0 );
     fieldsTable->setNumCols( 6 );
     GroupBox5Layout->addWidget( fieldsTable );
     OSGSceneViewLayout->addWidget( mainSplitter );
+    languageChange();
+    resize( QSize(667, 537).expandedTo(minimumSizeHint()) );
+    clearWState( WState_Polished );
 
     // signals and slots connections
     connect( treeListView, SIGNAL( rightButtonPressed(QListViewItem*,const QPoint&,int) ), this, SLOT( popupTreeMenu(QListViewItem*) ) );
@@ -125,11 +128,40 @@ OSGSceneView::OSGSceneView( QWidget* parent,  const char* name, WFlags fl )
     init();
 }
 
-/*  
+/*
  *  Destroys the object and frees any allocated resources
  */
 OSGSceneView::~OSGSceneView()
 {
     // no need to delete child widgets, Qt does it all for us
+}
+
+/*
+ *  Sets the strings of the subwidgets using the current
+ *  language.
+ */
+void OSGSceneView::languageChange()
+{
+    setCaption( tr( "Form1" ) );
+    GroupBox1->setTitle( tr( "Tree" ) );
+    treeListView->header()->setLabel( 0, tr( "Node Type" ) );
+    treeListView->header()->setLabel( 1, tr( "Core Type" ) );
+    treeListView->header()->setLabel( 2, tr( "Node Name" ) );
+    treeListView->header()->setLabel( 3, tr( "Core Name" ) );
+    treeListView->header()->setLabel( 4, tr( "Core USEed" ) );
+    GroupBox3->setTitle( tr( "Info" ) );
+    infoTable->horizontalHeader()->setLabel( 0, tr( "Value" ) );
+    infoTable->verticalHeader()->setLabel( 0, tr( "Node Type Name" ) );
+    infoTable->verticalHeader()->setLabel( 1, tr( "Node Name" ) );
+    infoTable->verticalHeader()->setLabel( 2, tr( "Core Type Name" ) );
+    infoTable->verticalHeader()->setLabel( 3, tr( "Core Name" ) );
+    infoTable->verticalHeader()->setLabel( 4, tr( "Core USEed count" ) );
+    GroupBox5->setTitle( tr( "Fields" ) );
+    fieldsTable->horizontalHeader()->setLabel( 0, tr( "Name" ) );
+    fieldsTable->horizontalHeader()->setLabel( 1, tr( "Type" ) );
+    fieldsTable->horizontalHeader()->setLabel( 2, tr( "Value" ) );
+    fieldsTable->horizontalHeader()->setLabel( 3, tr( "Cardinality" ) );
+    fieldsTable->horizontalHeader()->setLabel( 4, tr( "ID" ) );
+    fieldsTable->horizontalHeader()->setLabel( 5, tr( "Mask" ) );
 }
 
--- Examples/SceneViewer/OSGSceneView_qt.h
+++ Examples/SceneViewer/OSGSceneView_qt.h
@@ -1,30 +1,32 @@
 /****************************************************************************
 ** Form interface generated from reading ui file 'OSGSceneView_qt.ui'
 **
-** Created: Mon Sep 2 18:52:24 2002
-**      by:  The User Interface Compiler (uic)
+** Created: Fr 15. Aug 10:55:55 2003
+**      by: The User Interface Compiler ($Id: qt/main.cpp   3.1.2   edited Dec 19 11:45 $)
 **
 ** WARNING! All changes made in this file will be lost!
 ****************************************************************************/
+
 #ifndef OSGSCENEVIEW_H
 #define OSGSCENEVIEW_H
 
 #include <qvariant.h>
+#include <qwidget.h>
+#include <qpopupmenu.h>
 #include <OpenSG/OSGNode.h>
 #include <list>
-#include <qpopupmenu.h>
-#include <qwidget.h>
-class QVBoxLayout; 
-class QHBoxLayout; 
-class QGridLayout; 
+
+class QVBoxLayout;
+class QHBoxLayout;
+class QGridLayout;
+class QSplitter;
 class QGroupBox;
 class QListView;
 class QListViewItem;
-class QSplitter;
 class QTable;
 
 class OSGSceneView : public QWidget
-{ 
+{
     Q_OBJECT
 
 public:
@@ -40,7 +42,6 @@ public:
     QTable* infoTable;
     QGroupBox* GroupBox5;
     QTable* fieldsTable;
-
 
 public slots:
     virtual void addListItem( osg::NodePtr node, QListViewItem * parentItem );
@@ -62,11 +63,6 @@ public slots:
     virtual void removeView( QWidget * object );
 
 protected:
-    QHBoxLayout* OSGSceneViewLayout;
-    QHBoxLayout* GroupBox1Layout;
-    QHBoxLayout* GroupBox3Layout;
-    QHBoxLayout* GroupBox5Layout;
-
     QListViewItem *activeTreeItem;
     QListViewItem *rootTreeItem;
     QString filter;
@@ -75,6 +71,15 @@ protected:
     osg::NodePtr rootNode;
     std::list<QWidget *> viewList;
     bool statState;
+
+    QHBoxLayout* OSGSceneViewLayout;
+    QHBoxLayout* GroupBox1Layout;
+    QHBoxLayout* GroupBox3Layout;
+    QHBoxLayout* GroupBox5Layout;
+
+protected slots:
+    virtual void languageChange();
+
 };
 
 #endif // OSGSCENEVIEW_H
