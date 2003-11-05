d59faee1fe479d3736a4d1bbd26dd03d67cfbfda
dirk <dirk>
2003-11-05 21:00:45 +0000

    added some doc added CoredNodePtr

---
+++ Source/System/FieldContainer/OSGCoredNodePtr.h
@@ -0,0 +1,216 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *                     Copyright 2000-2002 by OpenSG Forum                   *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+#ifndef _OSGCOREDNODEPTR_H_
+#define _OSGCOREDNODEPTR_H_
+#ifdef __sgi
+#pragma once
+#endif
+
+#ifdef OSG_DOC_FILES_IN_MODULE
+/*! \file OSGCoredNodePtr.h
+    \ingroup GrpSystemFieldContainer
+ */
+#endif
+
+#include <OSGSystemDef.h>
+#include <OSGBaseTypes.h>
+#include <OSGNodePtr.h>
+#include <OSGNodeCore.h>
+
+OSG_BEGIN_NAMESPACE
+
+/*! \ingroup GrpSystemFieldContainer
+ */
+
+class OSG_SYSTEMLIB_DLLMAPPING CoredNodePtrBase
+{
+    /*==========================  PUBLIC  =================================*/
+  public: 
+
+    /*---------------------------------------------------------------------*/
+    /*! \name                   Constructors                               */
+    /*! \{                                                                 */
+  
+    CoredNodePtrBase(void);
+  
+    CoredNodePtrBase(NodePtr node);
+    
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                   Destructor                                 */
+    /*! \{                                                                 */
+ 
+    virtual ~CoredNodePtrBase();
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                      Access                                  */
+    /*! \{                                                                 */
+    
+    NodePtr node(void) const;
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+
+    /*=========================  PROTECTED  ===============================*/    
+  protected:
+  
+    /*---------------------------------------------------------------------*/
+    /*! \name                      Access                                  */
+    /*! \{                                                                 */
+
+    virtual NodeCorePtr getCoreV(void) const = 0;
+    
+    void setNode(const NodePtr &node);
+    
+    void setNode(const NullFieldContainerPtr &);
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+
+    /*==========================  PRIVATE  ================================*/
+  private:
+    
+    NodePtr _node;
+};
+
+/*! \ingroup GrpSystemFieldContainer
+ */
+
+template< class Core >
+class OSG_SYSTEMLIB_DLLMAPPING CoredNodePtr : public CoredNodePtrBase
+{
+    /*==========================  PUBLIC  =================================*/
+  public:
+  
+    /*---------------------------------------------------------------------*/
+    /*! \name                   Constructors                               */
+    /*! \{                                                                 */
+  
+    CoredNodePtr(void);
+    
+    explicit CoredNodePtr(const NodePtr& node);
+    
+    explicit CoredNodePtr(const NodeCorePtr& core);
+    
+    explicit CoredNodePtr(const typename Core::Ptr& core);
+     
+    CoredNodePtr(const CoredNodePtr<Core>& ptr);
+   
+    static CoredNodePtr<Core> create(void);
+     
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                   Destructor                                 */
+    /*! \{                                                                 */
+ 
+    virtual ~CoredNodePtr();
+    
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                      Access                                  */
+    /*! \{                                                                 */
+    
+    void coreChanged(void);
+  
+    typename Core::Ptr core(void) const;
+    
+    operator FieldContainerPtr(void) const;
+
+    typename Core::Ptr& operator->(void);
+    
+    /* Note: these only work for a RHS */
+    operator NodePtr();
+
+    operator typename Core::Ptr();
+
+    /* Assignment. Create a new CNP if necessary */
+    
+    CoredNodePtr<Core>& operator =(const NodePtr& node);
+
+    CoredNodePtr<Core>& operator =(const typename Core::Ptr& core);
+
+    CoredNodePtr<Core>& operator =(const CoredNodePtr<Core>& cnp);
+   
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+
+    /*=========================  PROTECTED  ===============================*/    
+  protected:
+  
+    /*---------------------------------------------------------------------*/
+    /*! \name                      Access                                  */
+    /*! \{                                                                 */
+
+    NodeCorePtr getCoreV(void) const;
+    
+    void setCore(const typename Core::Ptr &core);
+
+    void setCore(const NullFieldContainerPtr&);
+    
+    void updateNode(void);
+    
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+
+    /*==========================  PRIVATE  ================================*/
+  private:
+  
+    typename Core::Ptr _core;
+};
+
+// begin/endEdit functions. 
+
+template< class Core >
+inline
+void beginEditCP(const CoredNodePtr<Core> &objectP, 
+                       BitVector         whichField = FieldBits    ::AllFields,
+                       UInt32            origin     = ChangedOrigin::External);
+
+template< class Core >
+inline
+void endEditCP  (const CoredNodePtr<Core> &objectP, 
+                       BitVector         whichField = FieldBits    ::AllFields,
+                       UInt32            origin     = ChangedOrigin::External);
+
+OSG_END_NAMESPACE
+
+#include "OSGCoredNodePtr.inl"
+
+#define OSGCOREDNODEPTR_HEADER_CVSID "@(#)$Id: OSGCoredNodePtr.h,v 1.1 2003/11/05 21:00:46 dirk Exp $"
+
+#endif /* _OSGCOREDNODEPTR_H_ */
---
+++ Source/System/FieldContainer/OSGCoredNodePtr.inl
@@ -0,0 +1,289 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *                     Copyright 2000-2002 by OpenSG Forum                   *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+#ifdef OSG_DOC_FILES_IN_MODULE
+/*! \file OSGCoredNodePtr.inl
+    \ingroup GrpSystemFieldContainer
+ */
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+
+OSG_BEGIN_NAMESPACE
+
+
+CoredNodePtrBase::CoredNodePtrBase(void) : _node(NullFC)
+{}
+
+CoredNodePtrBase::CoredNodePtrBase(NodePtr node)
+{
+    setNode(node);
+}
+
+CoredNodePtrBase::~CoredNodePtrBase()
+{
+    setNode(NullFC);
+}
+
+NodePtr CoredNodePtrBase::node(void) const
+{ 
+    return _node; 
+}
+
+void CoredNodePtrBase::setNode(const NodePtr &node)
+{
+    if(_node == node)
+        return;
+
+    if(_node != NullFC)
+        subRefCP(_node);
+
+    _node = node;
+
+    if(_node != NullFC)
+        addRefCP(_node);
+}
+
+void CoredNodePtrBase::setNode(const NullFieldContainerPtr &)
+{
+    if(_node != NullFC)
+        subRefCP(_node);
+
+    _node = NullFC;
+}
+
+// CoredNodePtr methods
+
+template< class Core >
+CoredNodePtr<Core>::CoredNodePtr(void) : 
+    CoredNodePtrBase(NullFC), _core(NullFC)
+{}
+
+template< class Core >
+CoredNodePtr<Core>::CoredNodePtr(const NodePtr& node)
+{
+    setNode(node);
+    setCore(Core::Ptr::dcast(CoredNodePtrBase::node()->getCore()));
+}
+
+template< class Core >
+CoredNodePtr<Core>::CoredNodePtr(const NodeCorePtr& core)
+{
+    setCore(Core::Ptr::dcast(core));
+    updateNode();
+}
+
+template< class Core >
+CoredNodePtr<Core>::CoredNodePtr(const typename Core::Ptr& core)
+{
+    setCore(core);
+    updateNode();
+}
+
+template< class Core >
+CoredNodePtr<Core>::CoredNodePtr(const CoredNodePtr<Core>& ptr)
+{
+    setCore(ptr._core);
+    setNode(ptr.node());
+}
+
+template< class Core >
+CoredNodePtr<Core>::~CoredNodePtr()
+{
+    setCore(NullFC);
+    setNode(NullFC);
+}
+
+template< class Core >
+CoredNodePtr<Core> CoredNodePtr<Core>::create(void)
+{
+    return CoredNodePtr<Core>(Core::create());
+}
+
+template< class Core >
+void CoredNodePtr<Core>::coreChanged(void)
+{
+    setCore(node()->getCore());
+}
+
+template< class Core >
+typename Core::Ptr CoredNodePtr<Core>::core(void) const
+{
+#ifdef OSG_DEBUG
+    FFASSERT((node()->getCore() == _core), false, 
+              ("CoredNodePtr::operator Core::Ptr: core changed!"););
+#endif
+    return _core;
+}
+
+template< class Core >
+CoredNodePtr<Core>::operator FieldContainerPtr(void) const
+{
+#ifdef OSG_DEBUG
+    FFASSERT((node()->getCore() == _core), false, 
+              ("CoredNodePtr::operator Core::Ptr: core changed!"););
+#endif
+    return node();
+}
+
+template< class Core >
+typename Core::Ptr& CoredNodePtr<Core>::operator->(void)
+{
+#ifdef OSG_DEBUG
+    FFASSERT((node()->getCore() == _core), false, 
+              ("CoredNodePtr::operator Core::Ptr: core changed!"););
+#endif
+    return _core;
+}
+
+template< class Core >
+CoredNodePtr<Core>::operator NodePtr()
+{
+#ifdef OSG_DEBUG
+    FFASSERT((node()->getCore() == _core), false, 
+              ("CoredNodePtr::operator Core::Ptr: core changed!"););
+#endif
+    return node();
+}
+
+template< class Core >
+CoredNodePtr<Core>::operator typename Core::Ptr()
+{
+#ifdef OSG_DEBUG
+    FFASSERT((node()->getCore() == _core), false, 
+              ("CoredNodePtr::operator Core::Ptr: core changed!"););
+#endif
+    return _core;
+}
+
+/* Assignment. Create a new CNP if necessary */
+
+template< class Core >
+CoredNodePtr<Core>& CoredNodePtr<Core>::operator =(const NodePtr& node)
+{
+    setNode(node);
+    setCore(Core::Ptr::dcast(node->getCore()));
+    return *this;
+}
+
+template< class Core >
+CoredNodePtr<Core>& CoredNodePtr<Core>::operator =(const typename Core::Ptr& core)
+{
+    setCore(core);
+    updateNode();
+
+    return *this;
+}
+
+template< class Core >
+CoredNodePtr<Core>& CoredNodePtr<Core>::operator =(const CoredNodePtr<Core>& cnp)
+{
+    setNode(cnp.node());
+    setCore(cnp._core);
+    return *this;
+}
+
+template< class Core >
+NodeCorePtr CoredNodePtr<Core>::getCoreV(void) const
+{
+    return _core;
+}
+
+template< class Core >
+void CoredNodePtr<Core>::setCore(const typename Core::Ptr &core)
+{
+    if(_core == core)
+        return;
+
+    if(_core != NullFC)
+        subRefCP(_core);
+
+    _core = core;
+
+    if(_core != NullFC)
+        addRefCP(_core);
+}
+
+template< class Core >
+void CoredNodePtr<Core>::setCore(const NullFieldContainerPtr&)
+{
+    if(_core != NullFC)
+        subRefCP(_core);
+
+    _core = NullFC;
+}
+
+template< class Core >
+void CoredNodePtr<Core>::updateNode(void)
+{
+    if(node() == NullFC)
+    {
+        setNode(Node::create());
+    }
+
+    beginEditCP(node(), Node::CoreFieldMask);
+    node()->setCore(_core);
+    endEditCP(node(), Node::CoreFieldMask); 
+}
+
+
+// begin/endEdit functions. 
+
+template< class Core >
+inline
+void beginEditCP(const CoredNodePtr<Core> &objectP, 
+                       BitVector         whichField,
+                       UInt32            origin)
+{
+    beginEditCP(objectP.node(), whichField, origin);
+    beginEditCP(objectP.core(), whichField, origin);   
+}
+
+template< class Core >
+inline
+void endEditCP  (const CoredNodePtr<Core> &objectP, 
+                       BitVector         whichField,
+                       UInt32            origin)
+{
+    endEditCP(objectP.node(), whichField, origin);
+    endEditCP(objectP.core(), whichField, origin);   
+}
+
+OSG_END_NAMESPACE
+
+#define OSGCOREDNODEPTR_INLINE_CVSID "@(#)$Id: OSGCoredNodePtr.inl,v 1.1 2003/11/05 21:00:47 dirk Exp $"
+
