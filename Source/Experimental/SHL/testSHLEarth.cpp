706250609be36cb69ccca0918a3d0a9a181ad52f
a-m-z <a-m-z>
2004-06-09 15:21:16 +0000

    added Earth Shading Language Demo.

--- Source/Experimental/SHL/testSHLEarth.cpp
+++ Source/Experimental/SHL/testSHLEarth.cpp
@@ -1,7 +1,7 @@
 // OpenSG example: testSHL
 //
 // Demonstrates the use of the SHLChunk
-// Implements a simple bumpmapping via vertex and fragment shader.
+// Earth Shader Demo
 
 // Headers
 #include <GL/glut.h>
@@ -39,6 +39,7 @@ static SimpleSceneManager *_mgr;
 static NodePtr _scene;
 
 static SHLChunkPtr _shl = NullFC;
+static Int32 _animation = 1;
 
 // forward declaration so we can have the interesting stuff upfront
 int setupGLUT( int *argc, char *argv[] );
@@ -115,14 +116,15 @@ int main(int argc, char **argv)
     _shl = SHLChunk::create();
     beginEditCP(_shl);
         if(!_shl->readVertexProgram("Earth.vp"))
-            fprintf(stderr, "Couldn't read vertex program 'earth.vp'\n");
+            fprintf(stderr, "Couldn't read vertex program 'Earth.vp'\n");
         if(!_shl->readFragmentProgram("Earth.fp"))
-            fprintf(stderr, "Couldn't read fragment program 'earth.fp'\n");
+            fprintf(stderr, "Couldn't read fragment program 'Earth.fp'\n");
         _shl->setUniformParameter("EarthDay", 0);
         _shl->setUniformParameter("EarthNight", 1);
         _shl->setUniformParameter("EarthCloudGloss", 2);
-        // just a test ...
-        _shl->setUniformParameter("test", 0.1f);
+        _shl->setUniformParameter("season", 0.0f);
+        _shl->setUniformParameter("cos_time_0_2PI", -0.406652f);
+        _shl->setUniformParameter("sin_time_0_2PI", -0.913583f);
     endEditCP(_shl);
 
     beginEditCP(cmat);
@@ -137,7 +139,6 @@ int main(int argc, char **argv)
     _scene = Node::create();
 
     GeometryPtr geo = makeLatLongSphereGeo (100, 100, 1.0);
-    //GeometryPtr geo = makeTorusGeo(.8, 1.8, 128, 128);
     beginEditCP( geo, Geometry::MaterialFieldMask);
         geo->setMaterial(cmat);
     endEditCP(geo, Geometry::MaterialFieldMask);
@@ -161,22 +162,6 @@ int main(int argc, char **argv)
     _mgr->setWindow(gwin );
     _mgr->setRoot(_scene);
 
-    /*
-    // create point headlight
-    _mgr->turnHeadlightOff();
-    NodePtr headlight = _mgr->getHighlight();
-    PointLightPtr light    = PointLight::create();
-    beginEditCP(light);
-        light->setAmbient  (.3, .3, .3, 1);
-        light->setDiffuse  ( 1,  1,  1, 1);
-        light->setSpecular ( 1,  1,  1, 1);
-        light->setBeacon   (_mgr->getCamera()->getBeacon());
-    endEditCP(light);
-    beginEditCP(_scene);
-        _scene->setCore(light);
-    endEditCP(_scene);
-    */
-
     // show the whole scene
     _mgr->showAll();
 
@@ -193,8 +178,25 @@ int main(int argc, char **argv)
 // redraw the window
 void display(void)
 {
-  // render scene
-  _mgr->redraw();
+    static Real32 speed = 10000.0f;
+    static Real32 t = glutGet(GLUT_ELAPSED_TIME);
+    static Real32 t2 = 0.0;
+    
+    Real32 td = glutGet(GLUT_ELAPSED_TIME) - t;
+
+    if(td > speed)
+        t = glutGet(GLUT_ELAPSED_TIME);
+
+    if(_animation)
+        t2 = (2 * OSG::Pi / speed) * td;
+
+    beginEditCP(_shl, SHLChunk::ParametersFieldMask);
+        _shl->setUniformParameter("cos_time_0_2PI", osgcos(t2));
+        _shl->setUniformParameter("sin_time_0_2PI", osgsin(t2));
+    endEditCP(_shl, SHLChunk::ParametersFieldMask);
+
+    // render scene
+    _mgr->redraw();
 }
 
 // react to size changes
@@ -225,7 +227,7 @@ void motion(int x, int y)
 // react to keys
 void keyboard(unsigned char k, int x, int y)
 {
-    static Real32 count = 0; 
+    static Real32 season = 0.0f; 
     switch(k)
     {
         case 27:
@@ -236,11 +238,24 @@ void keyboard(unsigned char k, int x, int y)
             SceneFileHandler::the().write(_scene, "scene.osb.gz", true);
             printf("wrote scene.osb.gz\n");
         break;
-        case 't':
+        case 's':
+            if(season < 0.435)
+                season += 0.01;
+
             beginEditCP(_shl, SHLChunk::ParametersFieldMask);
-                _shl->setUniformParameter("test", count);
+                _shl->setUniformParameter("season", season);
             endEditCP(_shl, SHLChunk::ParametersFieldMask);
-            count += 0.01;
+        break;
+        case 'S':
+            if(season > -0.435)
+                season -= 0.01;
+
+            beginEditCP(_shl, SHLChunk::ParametersFieldMask);
+                _shl->setUniformParameter("season", season);
+            endEditCP(_shl, SHLChunk::ParametersFieldMask);
+        break;
+        case 'a':
+            _animation = 1 - _animation;
         break;
     }
 
@@ -253,13 +268,15 @@ int setupGLUT(int *argc, char *argv[])
     glutInit(argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
 
-    int winid = glutCreateWindow("OpenSG CG Shader");
+    int winid = glutCreateWindow("OpenSG SHL Shader");
 
     glutReshapeFunc(reshape);
     glutDisplayFunc(display);
     glutMouseFunc(mouse);
     glutMotionFunc(motion);
     glutKeyboardFunc(keyboard);
+
+    glutIdleFunc(display);
 
     return winid;
 }
