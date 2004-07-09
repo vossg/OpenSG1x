c798dadf7a4a7f1b6a97446a009351a6dff1ba13
a-m-z <a-m-z>
2004-07-09 08:20:35 +0000

    new fcdCompile with make NOGUI=true

--- Tools/fcdEdit/main.cpp
+++ Tools/fcdEdit/main.cpp
@@ -1,19 +1,19 @@
 
 
 #include <string>
-#include "FieldContainerViewCtl.h"
 
 #ifndef FCD_TERM
 
+#include "FieldContainerViewCtl.h"
 #include <qapplication.h>
 
 int main( int argc, char **argv )
 { 
-	int retCode = -1;
-	FieldContainer fc;
-	bool writeBase = false, writeFC = false;
-	std::string fcdFile, typeFile;
-	std::string writePath;
+    int retCode = -1;
+    FieldContainer fc;
+    bool writeBase = false, writeFC = false;
+    std::string fcdFile, typeFile;
+    std::string writePath;
 
 #ifdef WIN32
      const char sep = '\\';
@@ -21,117 +21,119 @@ int main( int argc, char **argv )
      const char sep = '/';
 #endif
 
-	// Win32 doesn't have getopt, DIY
-	do 
-	{
-		argc--, argv++;
-		if ( argc && argv[0][0] == '-' )
-		{
-    		switch (argv[0][1]) 
-    		{
-    		case 'd':
-    			if ( argc > 1 )
-    			{
-    				fcdFile = argv[1];
-    				argv++, argc--;
-    			}
-    			else
-    			{
-    				cerr << "fcdEdit: -d but no argument given!" <<endl;
-    				return 1;
-    			}				
-    			break;
-    		case 't':
-    			if ( argc > 1 )
-    			{
-    				typeFile = argv[1];
-    				argv++, argc--;
-    			}
-    			else
-    			{
-    				cerr << "fcdEdit: -t but no argument given!" <<endl;
-    				return 1;
-    			}				
-    			break;
-    		case 'b':
-    			writeBase = true;
-    			break;
-    		case 'f':
-    			writeFC = true;
-    			break;
-    		case 'p':
-    			if ( argc > 1 )
-    			{
-    				writePath = argv[1];
-    				argv++, argc--;
+    // Win32 doesn't have getopt, DIY
+    do 
+    {
+        argc--, argv++;
+        if ( argc && argv[0][0] == '-' )
+        {
+            switch (argv[0][1]) 
+            {
+            case 'd':
+                if ( argc > 1 )
+                {
+                    fcdFile = argv[1];
+                    argv++, argc--;
+                }
+                else
+                {
+                    cerr << "fcdEdit: -d but no argument given!" <<endl;
+                    return 1;
+                }                
+                break;
+            case 't':
+                if ( argc > 1 )
+                {
+                    typeFile = argv[1];
+                    argv++, argc--;
+                }
+                else
+                {
+                    cerr << "fcdEdit: -t but no argument given!" <<endl;
+                    return 1;
+                }                
+                break;
+            case 'b':
+                writeBase = true;
+                break;
+            case 'f':
+                writeFC = true;
+                break;
+            case 'p':
+                if ( argc > 1 )
+                {
+                    writePath = argv[1];
+                    argv++, argc--;
                     if(writePath[writePath.size()-1] != sep)
-						writePath += sep;
-    			}
-    			else
-    			{
-    				cerr << "fcdEdit: -p but no argument given!" <<endl;
-    				return 1;
-    			}				
-    			break;
-     		case 'h':
-    			cerr << "Usage: fcdEdit -d file.fcd -t file.ftd -bfh" << endl;
-    			cerr << "  -d file.fcd: load the field container description" << endl;
-    			cerr << "  -t file.ftd: load the field type description" << endl;
-    			cerr << "  -b write the field container base code files" << endl;
-    			cerr << "  -f write the field container code files" << endl;
-    			cerr << "  -p base code files output path" << endl;
-    			cerr << "  -h print usage" << endl;
-    			return 0;
-    			break;
-    		}
-    	}
-		else
-			break;
-	}
-	while (argc > 0);
-		
-	if ( argc )
-		fcdFile = argv[ 0 ];
-		
-	if (typeFile.empty())
-		Field::loadDefaultFieldTypeList();
-	else
-		Field::loadFieldTypeList(typeFile.c_str());
+                        writePath += sep;
+                }
+                else
+                {
+                    cerr << "fcdEdit: -p but no argument given!" <<endl;
+                    return 1;
+                }                
+                break;
+             case 'h':
+                cerr << "Usage: fcdEdit -d file.fcd -t file.ftd -bfh" << endl;
+                cerr << "  -d file.fcd: load the field container description" << endl;
+                cerr << "  -t file.ftd: load the field type description" << endl;
+                cerr << "  -b write the field container base code files" << endl;
+                cerr << "  -f write the field container code files" << endl;
+                cerr << "  -p base code files output path" << endl;
+                cerr << "  -h print usage" << endl;
+                return 0;
+                break;
+            }
+        }
+        else
+            break;
+    }
+    while (argc > 0);
+        
+    if ( argc )
+        fcdFile = argv[ 0 ];
+        
+    if (typeFile.empty())
+        Field::loadDefaultFieldTypeList();
+    else
+        Field::loadFieldTypeList(typeFile.c_str());
 
-	if (writeBase || writeFC) 
-	{
-		if (fcdFile.empty())
-			cerr << "ERROR: Can't write code without description" << endl;
-		else 
-		{
-			fc.readDesc(fcdFile.c_str());
-			fc.writeCode(writeBase,writeFC,writePath.c_str());
-			retCode = 0;
-		}
-	}
-	else 
-	{
-    	QApplication app( argc, argv );
-		FieldContainerViewCtl fCViewCtl( fcdFile.c_str() );
-		app.setMainWidget(&fCViewCtl);
-		fCViewCtl.show();
-		retCode = app.exec();
-	}
+    if (writeBase || writeFC) 
+    {
+        if (fcdFile.empty())
+            cerr << "ERROR: Can't write code without description" << endl;
+        else 
+        {
+            fc.readDesc(fcdFile.c_str());
+            fc.writeCode(writeBase,writeFC,writePath.c_str());
+            retCode = 0;
+        }
+    }
+    else 
+    {
+        QApplication app( argc, argv );
+        FieldContainerViewCtl fCViewCtl( fcdFile.c_str() );
+        app.setMainWidget(&fCViewCtl);
+        fCViewCtl.show();
+        retCode = app.exec();
+    }
 
-	return 0;
+    return 0;
 }
 
 #else // FCD_TERM
 
+#include "FieldContainer.h"
+
 int main( int argc, char **argv )
 { 
-	int retCode = -1;
-	FieldContainer fc;
-	bool writeBase = false, writeFC = false;
-	const char *optstring = "d:t:bfh";
-	std::string fcdFile, typeFile;
-	std::string writePath;
-	int opt = 0;
+    int retCode = -1;
+    FieldContainer fc;
+    bool writeBase = false, writeFC = false;
+    const char *optstring = "d:t:bfh";
+    std::string fcdFile, typeFile;
+    std::string writePath;
+    int opt = 0;
 
 #ifdef WIN32
      const char sep = '\\';
@@ -139,59 +141,131 @@ int main( int argc, char **argv )
      const char sep = '/';
 #endif
 
-	while (opt >= 0) {
-		opt = getopt(argc,argv,optstring);
-		switch (opt) {
-		case 'd':
-			fcdFile = optarg;
-			break;
-		case 't':
-			typeFile = optarg;
-			break;
-		case 'b':
-			writeBase = true;
-			break;
-		case 'f':
-			writeFC = true;
-			break;
-		case 'p':
-			writePath = optarg;
-			if(writePath[writePath.size()-1] != sep)
-				writePath += sep;
-			break;
-		case 'h':
-			cerr << "Usage: fcdEdit -d file.fcd -t file.ftd -bfh" << endl;
-			cerr << "  -d file.fcd: load the field container description" << endl;
-			cerr << "  -t file.ftd: load the field type description" << endl;
-			cerr << "  -b write the field container base code files" << endl;
-			cerr << "  -f write the field container code files" << endl;
-			cerr << "  -p base code files output path" << endl;
-			cerr << "  -h print usage" << endl;
-			return 0;
-			break;
-		}
-	}
-	
-	if ( optind < argc )
-		fcdFile = argv[ optind ];
-		
-	if (typeFile.empty())
-		Field::loadDefaultFieldTypeList();
-	else
-		Field::loadFieldTypeList(typeFile.c_str());
+#if 0
+    while (opt >= 0) {
+        opt = getopt(argc,argv,optstring);
+        switch (opt) {
+        case 'd':
+            fcdFile = optarg;
+            break;
+        case 't':
+            typeFile = optarg;
+            break;
+        case 'b':
+            writeBase = true;
+            break;
+        case 'f':
+            writeFC = true;
+            break;
+        case 'p':
+            writePath = optarg;
+            if(writePath[writePath.size()-1] != sep)
+                writePath += sep;
+            break;
+        case 'h':
+            cerr << "Usage: fcdEdit -d file.fcd -t file.ftd -bfh" << endl;
+            cerr << "  -d file.fcd: load the field container description" << endl;
+            cerr << "  -t file.ftd: load the field type description" << endl;
+            cerr << "  -b write the field container base code files" << endl;
+            cerr << "  -f write the field container code files" << endl;
+            cerr << "  -p base code files output path" << endl;
+            cerr << "  -h print usage" << endl;
+            return 0;
+            break;
+        }
+    }
+#endif
 
-	if (writeBase || writeFC) {
-		if (fcdFile.empty())
-			cerr << "ERROR: Can't write code without description" << endl;
-		else {
-			fc.readDesc(fcdFile.c_str());
-			fc.writeCode(writeBase,writeFC,writePath.c_str());
-			retCode = 0;
-		}
-	}
+    // Win32 doesn't have getopt, DIY
+    do 
+    {
+        argc--, argv++;
+        if ( argc && argv[0][0] == '-' )
+        {
+            switch (argv[0][1]) 
+            {
+            case 'd':
+                if ( argc > 1 )
+                {
+                    fcdFile = argv[1];
+                    argv++, argc--;
+                }
+                else
+                {
+                    cerr << "fcdEdit: -d but no argument given!" <<endl;
+                    return 1;
+                }                
+                break;
+            case 't':
+                if ( argc > 1 )
+                {
+                    typeFile = argv[1];
+                    argv++, argc--;
+                }
+                else
+                {
+                    cerr << "fcdEdit: -t but no argument given!" <<endl;
+                    return 1;
+                }                
+                break;
+            case 'b':
+                writeBase = true;
+                break;
+            case 'f':
+                writeFC = true;
+                break;
+            case 'p':
+                if ( argc > 1 )
+                {
+                    writePath = argv[1];
+                    argv++, argc--;
+                    if(writePath[writePath.size()-1] != sep)
+                        writePath += sep;
+                }
+                else
+                {
+                    cerr << "fcdEdit: -p but no argument given!" <<endl;
+                    return 1;
+                }                
+                break;
+             case 'h':
+                cerr << "Usage: fcdEdit -d file.fcd -t file.ftd -bfh" << endl;
+                cerr << "  -d file.fcd: load the field container description" << endl;
+                cerr << "  -t file.ftd: load the field type description" << endl;
+                cerr << "  -b write the field container base code files" << endl;
+                cerr << "  -f write the field container code files" << endl;
+                cerr << "  -p base code files output path" << endl;
+                cerr << "  -h print usage" << endl;
+                return 0;
+                break;
+            }
+        }
+        else
+            break;
+    }
+    while (argc > 0);
 
+    if ( argc )
+        fcdFile = argv[ 0 ];
+        
+    if (typeFile.empty())
+        Field::loadDefaultFieldTypeList();
+    else
+        Field::loadFieldTypeList(typeFile.c_str());
 
-	return 0;
+    if (writeBase || writeFC) 
+    {
+        if (fcdFile.empty())
+            cerr << "ERROR: Can't write code without description" << endl;
+        else 
+        {
+            fc.readDesc(fcdFile.c_str());
+            fc.writeCode(writeBase,writeFC,writePath.c_str());
+            retCode = 0;
+        }
+    }
+
+    return 0;
 }
 
 #endif
