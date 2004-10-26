028da57fac1322853aa4785d14f308f4ac756b34
vossg <vossg>
2004-10-26 09:32:51 +0000

    fixed : lib names

--- VSBuild/VS7/SystemLib/FlexLexer.h
+++ VSBuild/VS7/SystemLib/FlexLexer.h
@@ -1,4 +1,4 @@
-// $Header: /home/daffy/u0/vern/flex/RCS/FlexLexer.h,v 1.19 96/05/25 20:43:02 vern Exp $
+// /usr/local/src/Master//debian/flex/FlexLexer.h,v 1.3 2003/03/21 06:59:26 srivasta Exp
 
 // FlexLexer.h -- define interfaces for lexical analyzer classes generated
 //		  by flex
