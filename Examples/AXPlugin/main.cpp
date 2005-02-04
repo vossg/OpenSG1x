8a6c4c87570f246604cfd8bb00ef622eefeeea04
a-m-z <a-m-z>
2005-02-04 17:46:10 +0000

    added static support and version info.

--- Examples/AXPlugin/main.cpp
+++ Examples/AXPlugin/main.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-** $Id: main.cpp,v 1.2 2004/06/18 14:31:18 a-m-z Exp $
+** $Id: main.cpp,v 1.3 2005/02/04 17:46:12 a-m-z Exp $
 **
 ** Copyright (C) 1992-2000 Trolltech AS.  All rights reserved.
 **
@@ -27,6 +27,42 @@
 #include <OpenSG/OSGConfig.h>
 #include <OpenSG/OSGBaseFunctions.h>
 
+// for static linking
+#ifndef OSG_BUILD_DLL
+
+#include <OpenSG/OSGVRMLSceneFileType.h>
+#include <OpenSG/OSGOSGSceneFileType.h>
+#include <OpenSG/OSGNFIOSceneFileType.h>
+#include <OpenSG/OSGNFIOGeneric.h>
+#include <OpenSG/OSGNFIOGeometry.h>
+#include <OpenSG/OSGNFIOImage.h>
+
+#include <OpenSG/OSGPhongMaterial.h>
+#include <OpenSG/OSGDVRVolume.h>
+#include <OpenSG/OSGDVRVolumeTexture.h>
+#include <OpenSG/OSGDVRIsoSurface.h>
+#include <OpenSG/OSGDVRLookupTable.h>
+#include <OpenSG/OSGDVRSimpleLUTShader.h>
+
+#include <OpenSG/OSGSHLChunk.h>
+#include <OpenSG/OSGGeoPropColors.h>
+#include <OpenSG/OSGSurface.h>
+
+#include <OpenSG/OSGImageFileHandler.h>
+#include <OpenSG/OSGJPGImageFileType.h>
+#include <OpenSG/OSGTIFImageFileType.h>
+#include <OpenSG/OSGPNGImageFileType.h>
+#include <OpenSG/OSGGIFImageFileType.h>
+#include <OpenSG/OSGSGIImageFileType.h>
+#include <OpenSG/OSGDATImageFileType.h>
+#include <OpenSG/OSGMNGImageFileType.h>
+#include <OpenSG/OSGMTDImageFileType.h>
+#include <OpenSG/OSGPNMImageFileType.h>
+#include <OpenSG/OSGTGAImageFileType.h>
+
+#endif
+// -------------------------
+
 #ifdef OSG_GV_BETA
 #define EXPERIMENTAL_VRML_LOADER
 #endif
@@ -47,6 +83,52 @@ QAXFACTORY_DEFAULT( OSGAXPlugin,
             "{7687FBB9-FFA7-4cb9-A974-E32F15444CF5}"
             )
 
+static void initializeStatics(void)
+{
+    // for static linking
+#ifndef OSG_BUILD_DLL
+    
+    printf("VRML: %p\n", OSG::VRMLSceneFileType::the());
+    printf("OSG: %p\n", OSG::OSGSceneFileType::the());
+    printf("OSB: %p\n", OSG::NFIOSceneFileType::the());
+    new OSG::NFIOGeneric;
+    new OSG::NFIOGeometry;
+    new OSG::NFIOImage;
+
+    printf("Class id %d\n", OSG::DVRVolume::getClassTypeId());
+    printf("Class id %d\n", OSG::DVRVolumeTexture::getClassTypeId());
+    printf("Class id %d\n", OSG::DVRIsoSurface::getClassTypeId());
+    printf("Class id %d\n", OSG::DVRLookupTable::getClassTypeId());
+    printf("Class id %d\n", OSG::DVRSimpleLUTShader::getClassTypeId());
+
+    printf("Class id %d\n", OSG::PhongMaterial::getClassTypeId());
+    printf("Class id %d\n", OSG::SHLChunk::getClassTypeId());
+    printf("Class id %d\n", OSG::GeoColors3f::getClassTypeId());
+    printf("Class id %d\n", OSG::Surface::getClassTypeId());
+
+#ifdef OSG_WITH_JPG
+    printf("JPG: %p\n", OSG::JPGImageFileType::the());
+#endif
+#ifdef OSG_WITH_TIF
+    printf("TIF: %p\n", OSG::TIFImageFileType::the());
+#endif
+#ifdef OSG_WITH_PNG
+    printf("PNG: %p\n", OSG::PNGImageFileType::the());
+#endif
+#ifdef OSG_WITH_GIF
+    printf("GIF: %p\n", OSG::GIFImageFileType::the());
+#endif
+
+    printf("SGI: %p\n", OSG::SGIImageFileType::the());
+    printf("DAT: %p\n", OSG::DATImageFileType::the());
+    printf("MNG: %p\n", OSG::MNGImageFileType::the());
+    printf("MTD: %p\n", OSG::MTDImageFileType::the());
+    printf("PNM: %p\n", OSG::PNMImageFileType::the());
+    printf("TGA: %p\n", OSG::TGAImageFileType::the());
+    printf("Image %p\n", OSG::ImageFileHandler::the());
+
+#endif
+}
 
 /*
   The main program is here. 
@@ -54,6 +136,8 @@ QAXFACTORY_DEFAULT( OSGAXPlugin,
 
 int main(int argc, char **argv)
 {
+    initializeStatics();
+
     QApplication::setColorSpec( QApplication::CustomColor );
     QApplication a(argc,argv);
 
