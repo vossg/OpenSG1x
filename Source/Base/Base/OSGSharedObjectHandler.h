b960938dd59a14cf8a2005262afa63fea4dcf394
vossg <vossg>
2005-06-02 08:23:58 +0000

    fixed : cl.net2003 compile problems (OSG_BASE_DLLMAPPING missing)

--- Source/Base/Base/OSGSharedObjectHandler.h
+++ Source/Base/Base/OSGSharedObjectHandler.h
@@ -288,8 +288,8 @@ class OSG_BASE_DLLMAPPING SharedObjectHandler
     //   friend functions                                                    
     //-----------------------------------------------------------------------
 
-    friend  bool osgInit(Int32 argc, Char8 **argv);
-    friend  bool osgExit(void                    );
+    friend  OSG_BASE_DLLMAPPING bool osgInit(Int32 argc, Char8 **argv);
+    friend  OSG_BASE_DLLMAPPING bool osgExit(void                    );
 
     //-----------------------------------------------------------------------
     //   class variables                                                     
