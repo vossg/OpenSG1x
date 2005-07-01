d9a14e7e096a1e54fbae447403ca9c85566d0f0e
a-m-z <a-m-z>
2005-07-01 19:42:22 +0000

    added test prg for invalid ptr check.

---
+++ Source/System/FieldContainer/testInvalidPtr.cpp
@@ -0,0 +1,51 @@
+
+#define OSG_INVALID_PTR_CHECK
+
+
+#include <OSGConfig.h>
+
+#include <iostream>
+
+#include <string>
+#include <map>
+#include <OSGFieldContainerFactory.h>
+#include <OSGSFSysTypes.h>
+#include <OSGNode.h>
+#include <OSGNodeCore.h>
+#include <OSGThread.h>
+#include <OSGAttachment.h>
+#include <OSGMFVecTypes.h>
+#include <OSGBaseFunctions.h>
+#include <OSGFieldContainer.h>
+#include <OSGGroup.h>
+
+#include <OSGGeometry.h>
+
+#include <OSGSFFieldContainerPtr.h>
+#include <OSGMFFieldContainerPtr.h>
+#include <OSGSimpleGeometry.h>
+
+OSG_USING_NAMESPACE
+
+int main (int argc, char *argv[])
+{
+    osgInit(argc, argv);
+
+    for(UInt32 i=0;i<20;++i)
+    {
+        NodePtr planeNode = makePlane(2,2, 1,1);
+        addRefCP(planeNode);
+        subRefCP(planeNode);
+
+#ifdef OSG_INVALID_PTR_CHECK
+        // now call subRefCP on a invalid pointer!
+        subRefCP(planeNode);
+#endif
+    }
+
+    NodePtr planeNode = makePlane(2,2, 1,1);
+    addRefCP(planeNode);
+    subRefCP(planeNode);
+
+    return 0;
+}
