7fdff77efd1695dbc7d92213171596e530cd53cb
dirk <dirk>
2005-07-21 23:51:44 +0000

    added example for integrating OpenGL rendering in OpenSg image

--- Source/System/Window/testPassiveWindow.cpp
+++ Source/System/Window/testPassiveWindow.cpp
@@ -5,10 +5,12 @@
 #include <OSGSimpleGeometry.h>
 #include <OSGPassiveWindow.h>
 #include <OSGSimpleSceneManager.h>
+#include <OSGTransform.h>
 
 OSG_USING_NAMESPACE
 
 SimpleSceneManager *mgr;
+TransformNodePtr trans;
 
 int width,height;
 
@@ -35,7 +37,17 @@ void display(void)
     glPopMatrix();
     
     // now do the OpenSG part
+    
+    // Move the object a little
+    
+    beginEditCP(trans);
+    Matrix m;
+    m.setTransform(Vec3f(sin(f/100), 0, cos(f/100)));
+    trans->setMatrix(m);
+    endEditCP(trans);
 
+    // and draw it
+    
     glPushMatrix();
     glMatrixMode(GL_PROJECTION);
     glPushMatrix();
@@ -51,6 +63,67 @@ void display(void)
     glMatrixMode(GL_MODELVIEW);
     glPopMatrix();
 
+    // And a little more OpenGL
+
+    NodePtr node = trans.node()->getChild(0);
+    
+    ViewportPtr port = mgr->getWindow()->getPort(0);
+    CameraPtr cam = port->getCamera();
+    int w = port->getPixelWidth(), h = port->getPixelHeight();
+    Matrix proj, view;
+    
+    cam->getProjection(proj, w, h);
+    cam->getViewing(view, w, h);
+    
+    glMatrixMode(GL_PROJECTION);
+    glPushMatrix();
+    glLoadMatrixf(proj.getValues());
+    
+    glMatrixMode(GL_MODELVIEW);     
+    glPushMatrix();
+    glLoadMatrixf(view.getValues());
+    glMultMatrixf(node->getToWorld().getValues());
+
+    glPushAttrib(GL_ALL_ATTRIB_BITS);
+    glViewport(0,0,w,h);
+
+    
+    Pnt3f low, high;
+    node->getVolume().getBounds(low,high);
+    
+    glColor3f(fmod(f/2000,1), fmod(f/1500,1), fmod(f/800,1));
+    
+    glBegin(GL_LINE_LOOP);
+    glVertex3f( low[0],  low[1],  low[2]);
+    glVertex3f(high[0],  low[1],  low[2]);
+    glVertex3f(high[0], high[1],  low[2]);
+    glVertex3f( low[0], high[1],  low[2]);
+    glVertex3f( low[0],  low[1],  low[2]);
+
+    glVertex3f( low[0],  low[1], high[2]);
+    glVertex3f(high[0],  low[1], high[2]);
+    glVertex3f(high[0], high[1], high[2]);
+    glVertex3f( low[0], high[1], high[2]);
+    glVertex3f( low[0],  low[1], high[2]);    
+    glEnd();
+    
+    glBegin(GL_LINES);
+    glVertex3f(high[0],  low[1],  low[2]);
+    glVertex3f(high[0],  low[1], high[2]);
+    
+    glVertex3f(high[0], high[1],  low[2]);
+    glVertex3f(high[0], high[1], high[2]);
+    
+    glVertex3f( low[0], high[1],  low[2]);
+    glVertex3f( low[0], high[1], high[2]);
+    glEnd();
+
+    glPopAttrib();
+    glMatrixMode(GL_PROJECTION);
+    glPopMatrix();
+    glMatrixMode(GL_MODELVIEW);
+    glPopMatrix();
+    
     // all done, swap    
     glutSwapBuffers();
 }
@@ -115,14 +188,21 @@ int main(int argc, char **argv)
     pwin->init();
 
     // create the scene
+    
+    trans = Transform::create();
+    
     NodePtr scene = makeTorus(.5, 2, 16, 16);
 
+    beginEditCP(trans.node());
+    trans.node()->addChild(scene);
+    endEditCP(trans.node());
+    
     // create the SimpleSceneManager helper
     mgr = new SimpleSceneManager;
 
     // tell the manager what to manage
     mgr->setWindow(pwin );
-    mgr->setRoot  (scene);
+    mgr->setRoot  (trans.node());
     
     // take the viewport  and make it smaller
     ViewportPtr vp = pwin->getPort(0);
