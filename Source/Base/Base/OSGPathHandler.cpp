5c2771a7a1046324eb7860417a58c06a556c41cb
dirk <dirk>
2005-07-26 19:18:09 +0000

    moved some messages to INFO

--- Source/Base/Base/OSGPathHandler.cpp
+++ Source/Base/Base/OSGPathHandler.cpp
@@ -108,7 +108,7 @@ std::string PathHandler::findFile(const Char8 *fileName)
     {
         if((pType & TypeMask) == AbsPath)
         {
-            SNOTICE << "Check abs : " << tmpList.front() << std::endl;
+            SINFO << "Check abs : " << tmpList.front() << std::endl;
 
             if(File::tstAttr(tmpList.front().c_str(),
                              AccessFlags::IsReadable))
@@ -124,7 +124,7 @@ std::string PathHandler::findFile(const Char8 *fileName)
 
                 returnValue.append(tmpList.front());
 
-                SNOTICE << "Check base : " << returnValue << std::endl;
+                SINFO << "Check base : " << returnValue << std::endl;
 
                 if(File::tstAttr(returnValue.c_str(),
                                  AccessFlags::IsReadable) == false)
@@ -144,7 +144,7 @@ std::string PathHandler::findFile(const Char8 *fileName)
                     returnValue.assign(*iter);
                     returnValue.append(tmpList.front());
 
-                    SNOTICE << "Check from pl : " << returnValue << std::endl;
+                    SINFO << "Check from pl : " << returnValue << std::endl;
 
                     if(File::tstAttr(returnValue.c_str(),
                                      AccessFlags::IsReadable) == true)
