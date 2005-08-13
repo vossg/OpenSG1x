fb54bce497339d1343e1f848ed7c854f1ec711ee
dirk <dirk>
2005-08-13 23:20:01 +0000

    typo

--- dist/start_db_cygwin.bat
+++ dist/start_db_cygwin.bat
@@ -4,7 +4,7 @@ SET MAKE_MODE=UNIX
 set USER=dreiners
 set HOME=/cygdrive/c/users/dreiners
 SET PATH=c:\users\dreiners\cygwin\bin;c:\users\dreiners\cygwin\usr\X11R6\bin;%PATH%;c:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE;c:\Program Files\Microsoft Visual Studio .NET 2003\Common7\Tools
-vsvars32.bat
+call vsvars32.bat
 
 c:
 cd \users\dreiners\OpenSG_db
