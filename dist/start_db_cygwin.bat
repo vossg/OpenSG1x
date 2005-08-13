7dfd20feffe9f26f99259039cc58a65802b253c9
dirk <dirk>
2005-08-13 22:48:58 +0000

    minor changes

--- dist/start_db_cygwin.bat
+++ dist/start_db_cygwin.bat
@@ -1,11 +1,12 @@
 @echo off
 
 SET MAKE_MODE=UNIX
-set USER=reiners
-set HOME=/home/reiners
-SET PATH=f:\cygwin\bin;%PATH%
+set USER=dreiners
+set HOME=/cygdrive/c/users/dreiners
+SET PATH=c:\users\dreiners\cygwin\bin;c:\users\dreiners\cygwin\usr\X11R6\bin;%PATH%;c:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE;c:\Program Files\Microsoft Visual Studio .NET 2003\Common7\Tools
+vsvars32.bat
 
-f:
-cd \user\reiners\opensg_dailybuild
+c:
+cd \users\dreiners\OpenSG_db
 
 bash dailybuild_copy
