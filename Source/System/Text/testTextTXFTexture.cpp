cc890d11ddad98e578f5a91f65b31ede4e754286
pdaehne <pdaehne>
2006-07-25 17:48:28 +0000

    Extended user interface to be able to change almost all parameters of TXF fonts

--- Source/System/Text/testTextTXFTexture.cpp
+++ Source/System/Text/testTextTXFTexture.cpp
@@ -18,6 +18,8 @@
 #include "OSGTextTXFGlyph.h"
 #include "OSGTextFaceFactory.h"
 
+#include <sstream>
+
 // Activate the OpenSG namespace
 OSG_USING_NAMESPACE
 using namespace std;
@@ -30,7 +32,21 @@ using namespace std;
 #define COMMAND_STYLE_BOLD 302
 #define COMMAND_STYLE_ITALIC 303
 #define COMMAND_STYLE_BOLDITALIC 304
-#define COMMAND_WRITE_TO_FILE 401
+#define COMMAND_SIZE_INC1 401
+#define COMMAND_SIZE_INC10 402
+#define COMMAND_SIZE_DEC1 403
+#define COMMAND_SIZE_DEC10 404
+#define COMMAND_GAP_INC1 501
+#define COMMAND_GAP_DEC1 502
+#define COMMAND_TEXTUREWIDTH_AUTO 601
+#define COMMAND_TEXTUREWIDTH_64 602
+#define COMMAND_TEXTUREWIDTH_128 603
+#define COMMAND_TEXTUREWIDTH_256 604
+#define COMMAND_TEXTUREWIDTH_512 605
+#define COMMAND_TEXTUREWIDTH_1024 606
+#define COMMAND_TEXTUREWIDTH_2048 607
+#define COMMAND_TEXTUREWIDTH_4096 608
+#define COMMAND_WRITE_TO_FILE 901
 
 // The SimpleSceneManager to manage simple applications
 SimpleSceneManager *mgr;
@@ -38,14 +54,17 @@ SimpleSceneManager *mgr;
 NodePtr scene;
 
 SimpleStatisticsForegroundPtr statfg;
-StatElemDesc<OSG::StatStringElem> foobarDesc("foobar", "foobar");
 StatElemDesc<OSG::StatStringElem> familyDesc("family", "The font family");
 StatElemDesc<OSG::StatStringElem> styleDesc("style", "The font style");
+StatElemDesc<OSG::StatIntElem> sizeDesc("size", "The height of the characters");
+StatElemDesc<OSG::StatIntElem> gapDesc("gap", "The gap between characters");
+StatElemDesc<OSG::StatStringElem> textureSizeDesc("textureSize", "The size of the texture");
 
 TextTXFFace *face = 0;
-string family = "SANS";
+string family;
 vector<string> families;
 TextFace::Style style = TextFace::STYLE_PLAIN;
+TextTXFParam param;
 string filename;
 
 int mainMenuID;
@@ -145,15 +164,17 @@ NodePtr createMetrics(TextTXFFace *face, UInt32 width, UInt32 height)
 void updateFace()
 {
     // Try to create new face
-    TextTXFParam param;
-    //param.setCharacters("Aba.AbaAA.\xce\xb1");
-    //param.size = 20;
-    TextTXFFace *newFace = TextTXFFace::create(family, style, param);
-    if (newFace == 0)
+    if (family.empty() == false)
+    {
+        TextTXFFace *newFace = TextTXFFace::create(family, style, param);
+        if (newFace == 0)
+            return;
+        subRefP(face);
+        face = newFace;
+        addRefP(face);
+    }
+    if (face == 0)
         return;
-    subRefP(face);
-    face = newFace;
-    addRefP(face);
 
     // Update information on the screen
     family = face->getFamily();
@@ -186,8 +207,14 @@ void updateFace()
             filename.append("-BoldItalic.txf");
             break;
     }
+    statfg->getCollector().getElem(sizeDesc)->set(face->getParam().size);
+    statfg->getCollector().getElem(gapDesc)->set(face->getParam().gap);
+    ImagePtr imagePtr = face->getTexture();
+    ostringstream os;
+    os << imagePtr->getWidth() << 'x' << imagePtr->getHeight();
+    statfg->getCollector().getElem(textureSizeDesc)->set(os.str());
     glutSetMenu(mainMenuID);
-    glutChangeToMenuEntry(3, (string("Write to ") + filename).c_str(), COMMAND_WRITE_TO_FILE);
+    glutChangeToMenuEntry(6, (string("Write to ") + filename).c_str(), COMMAND_WRITE_TO_FILE);
 }
 
 void updateScene()
@@ -295,6 +322,9 @@ int main(int argc, char **argv)
     statfg->setColor(Color4f(0,1,0,0.9));
     statfg->addElement(familyDesc, "Family: %s");
     statfg->addElement(styleDesc, "Style: %s");
+    statfg->addElement(sizeDesc, "Size: %i");
+    statfg->addElement(gapDesc, "Gap: %i");
+    statfg->addElement(textureSizeDesc, "Texture Size: %s");
     endEditCP(statfg);
 
     // Create the background
@@ -303,6 +333,19 @@ int main(int argc, char **argv)
     bg->setColor(Color3f(0.1, 0.1, 0.5));
     endEditCP(bg);
 
+    if (argc > 1)
+    {
+        face = TextTXFFace::createFromFile(argv[1]);
+        if (face == 0)
+            family = "SANS";
+        else
+        {
+            addRefP(face);
+            family.erase();
+        }
+    }
+    else
+        family = "SANS";
     updateFace();
     updateScene();
 
@@ -411,6 +454,71 @@ void menu(int command)
             style = TextFace::STYLE_BOLDITALIC;
             updateFace();
             break;
+        case COMMAND_SIZE_INC1:
+            param.size += 1;
+            updateFace();
+            break;
+        case COMMAND_SIZE_INC10:
+            param.size += 10;
+            updateFace();
+            break;
+        case COMMAND_SIZE_DEC1:
+            if (param.size <= 1)
+                param.size = 1;
+            else
+                param.size -= 1;
+            updateFace();
+            break;
+        case COMMAND_SIZE_DEC10:
+            if (param.size <= 11)
+                param.size = 1;
+            else
+                param.size -= 10;
+            updateFace();
+            break;
+        case COMMAND_GAP_INC1:
+            param.gap += 1;
+            updateFace();
+            break;
+        case COMMAND_GAP_DEC1:
+            if (param.gap <= 0)
+                param.gap = 0;
+            else
+                param.gap -= 1;
+            updateFace();
+            break;
+        case COMMAND_TEXTUREWIDTH_AUTO:
+            param.textureWidth = 0;
+            updateFace();
+            break;
+        case COMMAND_TEXTUREWIDTH_64:
+            param.textureWidth = 64;
+            updateFace();
+            break;
+        case COMMAND_TEXTUREWIDTH_128:
+            param.textureWidth = 128;
+            updateFace();
+            break;
+        case COMMAND_TEXTUREWIDTH_256:
+            param.textureWidth = 256;
+            updateFace();
+            break;
+        case COMMAND_TEXTUREWIDTH_512:
+            param.textureWidth = 512;
+            updateFace();
+            break;
+        case COMMAND_TEXTUREWIDTH_1024:
+            param.textureWidth = 1024;
+            updateFace();
+            break;
+        case COMMAND_TEXTUREWIDTH_2048:
+            param.textureWidth = 2048;
+            updateFace();
+            break;
+        case COMMAND_TEXTUREWIDTH_4096:
+            param.textureWidth = 4096;
+            updateFace();
+            break;
         case COMMAND_WRITE_TO_FILE:
             face->writeToFile(filename);
             break;
@@ -454,10 +562,33 @@ int setupGLUT(int *argc, char *argv[])
     glutAddMenuEntry("Bold", COMMAND_STYLE_BOLD);
     glutAddMenuEntry("Italic", COMMAND_STYLE_ITALIC);
     glutAddMenuEntry("Bold + Italic", COMMAND_STYLE_BOLDITALIC);
+    int sizeMenuID = glutCreateMenu(menu);
+    glutSetMenu(sizeMenuID);
+    glutAddMenuEntry("Increment size by 1", COMMAND_SIZE_INC1);
+    glutAddMenuEntry("Increment size by 10", COMMAND_SIZE_INC10);
+    glutAddMenuEntry("Decrement size by 1", COMMAND_SIZE_DEC1);
+    glutAddMenuEntry("Decrement size by 10", COMMAND_SIZE_DEC10);
+    int gapMenuID = glutCreateMenu(menu);
+    glutSetMenu(gapMenuID);
+    glutAddMenuEntry("Increment gap by 1", COMMAND_GAP_INC1);
+    glutAddMenuEntry("Decrement gap by 1", COMMAND_GAP_DEC1);
+    int textureWidthMenuID = glutCreateMenu(menu);
+    glutSetMenu(textureWidthMenuID);
+    glutAddMenuEntry("Automatic", COMMAND_TEXTUREWIDTH_AUTO);
+    glutAddMenuEntry("64", COMMAND_TEXTUREWIDTH_64);
+    glutAddMenuEntry("128", COMMAND_TEXTUREWIDTH_128);
+    glutAddMenuEntry("256", COMMAND_TEXTUREWIDTH_256);
+    glutAddMenuEntry("512", COMMAND_TEXTUREWIDTH_512);
+    glutAddMenuEntry("1024", COMMAND_TEXTUREWIDTH_1024);
+    glutAddMenuEntry("2048", COMMAND_TEXTUREWIDTH_2048);
+    glutAddMenuEntry("4096", COMMAND_TEXTUREWIDTH_4096);
     mainMenuID = glutCreateMenu(menu);
     glutSetMenu(mainMenuID);
     glutAddSubMenu("Family", familyMenuID);
     glutAddSubMenu("Style", styleMenuID);
+    glutAddSubMenu("Size", sizeMenuID);
+    glutAddSubMenu("Gap", gapMenuID);
+    glutAddSubMenu("TextureWidth", textureWidthMenuID);
     glutAddMenuEntry("Write to ", COMMAND_WRITE_TO_FILE);
     glutAttachMenu(GLUT_RIGHT_BUTTON);
 
