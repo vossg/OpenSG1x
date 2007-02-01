cd3894731ed45d677620ec512acb2f05ce4070e8
a-m-z <a-m-z>
2007-02-01 21:57:39 +0000

    removed patch conflicts with the other makeCoredNode method.

--- Source/System/FieldContainer/OSGNodePtr.h
+++ Source/System/FieldContainer/OSGNodePtr.h
@@ -78,21 +78,6 @@ NodePtr makeCoredNode(typename Core::Ptr *coreP)
     return n;
 }
 
-template <class Core, class CoreParentPtr> inline
-NodePtr makeCoredNode(FCPtr<CoreParentPtr, Core> *coreP)
-{
-    NodePtr             n = Node::create();
-    typename Core::Ptr  c = Core::create();
-
-    beginEditCP(n, Node::CoreFieldMask);
-    n->setCore(c);
-    endEditCP(n, Node::CoreFieldMask);
-
-    if(coreP != NULL)
-        *coreP = c;
-
-    return n;
-}
 OSG_END_NAMESPACE
 
 #endif /* _OSGNODEPTR_H_ */
