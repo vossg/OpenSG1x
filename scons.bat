6917595c376b867b4cd51a4ebb38efc4e1257d87
a-m-z <a-m-z>
2007-03-19 11:25:17 +0000

    added -O option many thanks to Marcus Lindblom for the hint.

--- scons.bat
+++ scons.bat
@@ -1,6 +1,6 @@
 @echo off
 
-python SConsLocal/scons.py %*
+python -O SConsLocal/scons.py %*
 
 if NOT "%COMSPEC%" == "%SystemRoot%\system32\cmd.exe" goto endscons
 if errorlevel 9009 echo Did you install python from www.python.org? Or perhaps you need to add python to your PATH!
