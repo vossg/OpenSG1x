d65ce8a0d101b67242440aa517391fe946f6bbad
neumannc <neumannc>
2007-04-27 14:04:24 +0000

    fixed: sync animation thread to app thread after scene construction.

--- Doc/tutorial/progs/13multithreading2.cpp
+++ Doc/tutorial/progs/13multithreading2.cpp
@@ -12,59 +12,70 @@ using namespace std;
 
 SimpleSceneManager *mgr;
 NodePtr scene;
+
 //we will store the transformation globally - this
 //is not necessary, but comfortable
 TransformPtr trans;
 Thread* animationThread;
+Thread* applicationThread;
 Barrier *syncBarrier;
 
-int setupGLUT( int *argc, char *argv[] );
+int setupGLUT(int *argc, char *argv[]);
 
-NodePtr createScenegraph(){
+NodePtr createScenegraph()
+{
     // the scene must be created here
-	NodePtr n = makeTorus(.5,2,16,16);
-	
-	//add a simple Transformation
-	trans = Transform::create();
-	beginEditCP(trans);
-		Matrix m;
-		m.setIdentity();
-		trans->setMatrix(m);
-	endEditCP(trans);
-	
-	NodePtr transNode = Node::create();
-	beginEditCP(transNode);
-		transNode->setCore(trans);
-		transNode->addChild(n);
-	endEditCP(transNode);
-	
-	return transNode;
+    NodePtr n = makeTorus(.5,2,16,16);
+    
+    //add a simple Transformation
+    trans = Transform::create();
+    beginEditCP(trans);
+        Matrix m;
+        m.setIdentity();
+        trans->setMatrix(m);
+    endEditCP(trans);
+    
+    NodePtr transNode = Node::create();
+    beginEditCP(transNode);
+        transNode->setCore(trans);
+        transNode->addChild(n);
+    endEditCP(transNode);
+    
+    return transNode;
 }
 
 //this function will run in a thread and simply will
 //rotate the cube by setting a new transformation matrix
-void rotate(void *args){
-	// we won't stop calculating new matrices....
-	while(true){
-		Real32 time = glutGet(GLUT_ELAPSED_TIME);
-		Matrix m;
-		m.setIdentity();
-		m.setRotate(Quaternion(Vec3f(0,1,0), time/1000));
-		
-		beginEditCP(trans);
-			trans->setMatrix(m);
-		endEditCP(trans);
-		// nothing unusual until here
-		
-		//well that's new...
-		
-		//wait until two threads are cought in the
-		//same barrier
-		syncBarrier->enter(2);
-		
-		//just the same again
-		syncBarrier->enter(2);
-	}
+void rotate(void *args)
+{
+    // sync this thread to the main thread, i.e. pull in all changes done
+    // during scene construction
+    syncBarrier->enter(2);
+    applicationThread->getChangeList()->applyAndClear();
+    syncBarrier->enter(2);
+    
+    // we won't stop calculating new matrices....
+    while(true)
+    {
+        Real32 time = glutGet(GLUT_ELAPSED_TIME);
+        Matrix m;
+        m.setIdentity();
+        m.setRotate(Quaternion(Vec3f(0,1,0), time/1000));
+        
+        beginEditCP(trans);
+            trans->setMatrix(m);
+        endEditCP(trans);
+        // nothing unusual until here
+        
+        //well that's new...
+        
+        //wait until two threads are cought in the
+        //same barrier
+        syncBarrier->enter(2);    // barrier (1)
+        
+        //just the same again
+        syncBarrier->enter(2);    // barrier (2)
+    }
 }
 
 int main(int argc, char **argv)
@@ -72,7 +83,7 @@ int main(int argc, char **argv)
  #if OSG_MINOR_VERSION > 2
     ChangeList::setReadWriteDefault();
 #endif
-   osgInit(argc,argv);
+    osgInit(argc,argv);
         
     int winid = setupGLUT(&argc, argv);
     GLUTWindowPtr gwin= GLUTWindow::create();
@@ -80,24 +91,31 @@ int main(int argc, char **argv)
     gwin->init();
 
     scene =createScenegraph();
-	
-	//create the barrier, that will be used to
-	//synchronize threads
-	
-	//instead of NULL you could provide a name
-	syncBarrier = Barrier::get(NULL);
+    
+    //create the barrier, that will be used to
+    //synchronize threads
+    
+    //instead of NULL you could provide a name
+    syncBarrier = Barrier::get(NULL);
 
     mgr = new SimpleSceneManager;
     mgr->setWindow(gwin );
     mgr->setRoot  (scene);
     mgr->showAll();
-	
-	//create the thread that will run generation of new matrices
-	animationThread = dynamic_cast<Thread *>(ThreadManager::the()->getThread("anim"));
+    
+    // store a pointer to the application thread
+    applicationThread = dynamic_cast<Thread *>(ThreadManager::getAppThread());
+    
+    //create the thread that will run generation of new matrices
+    animationThread = dynamic_cast<Thread *>(ThreadManager::the()->getThread("anim"));
 
-	//do it...
-	animationThread->runFunction(rotate, 1, NULL);
-	
+    //do it...
+    animationThread->runFunction(rotate, 1, NULL);
+    
+    // wait for animationThread to complete its sync
+    syncBarrier->enter(2);
+    syncBarrier->enter(2);
+    
     glutMainLoop();
 
     return 0;
@@ -111,23 +129,23 @@ void reshape(int w, int h)
 
 void display(void)
 {
-	// we wait here until the animation thread enters
-	//the first barrier
-	syncBarrier->enter(2);
-	
-	//now we sync data
-	animationThread->getChangeList()->applyAndClear();
-	
-	// and again
-	syncBarrier->enter(2);
-	
-	// !!!! Attention
-	// you will find a more detailed description
-	// of what's going on here in the documentation
-	// itself!
-	
-	// now render...
-	mgr->redraw();
+    // we wait here until the animation thread enters
+    // barrier (1)
+    syncBarrier->enter(2);
+    
+    //now we sync data
+    animationThread->getChangeList()->applyAndClear();
+    
+    // now wait for animation thread to enter barrier (2)
+    syncBarrier->enter(2);
+    
+    // !!!! Attention
+    // you will find a more detailed description
+    // of what's going on here in the documentation
+    // itself!
+    
+    // now render...
+    mgr->redraw();
 }
 
 void mouse(int button, int state, int x, int y)
