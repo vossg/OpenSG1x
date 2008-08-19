e53fcf35115aed3a166c5f21bb8221a6e3f3b820
neumannc <neumannc>
2008-08-19 15:25:07 +0000

    added: MultiSwitch core
           Many thanks to Johannes Brunen for the code!

---
+++ Source/System/NodeCores/Groups/Misc/OSGMultiSwitch.cpp
@@ -0,0 +1,296 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *               Copyright (C) 2000-2002 by the OpenSG Forum                 *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+//---------------------------------------------------------------------------
+//  Includes
+//---------------------------------------------------------------------------
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <algorithm>
+
+#include <OSGConfig.h>
+
+#include "OSGMultiSwitch.h"
+
+#include <OSGDrawAction.h>
+#include <OSGRenderAction.h>
+#include <OSGIntersectAction.h>
+OSG_BEGIN_NAMESPACE
+
+/***************************************************************************\
+ *                            Description                                  *
+\***************************************************************************/
+
+/*! \class osg::MultiSwitch
+
+*/
+
+/***************************************************************************\
+ *                           Class variables                               *
+\***************************************************************************/
+
+/***************************************************************************\
+ *                           Class methods                                 *
+\***************************************************************************/
+
+void MultiSwitch::initMethod (void)
+{
+    DrawAction::registerEnterDefault(
+        getClassType(),
+        osgTypedMethodFunctor2BaseCPtrRef<Action::ResultE,
+                                          MultiSwitchPtr       ,
+                                          CNodePtr        ,
+                                          Action         *>(&MultiSwitch::draw));
+
+    RenderAction::registerEnterDefault(
+        getClassType(),
+        osgTypedMethodFunctor2BaseCPtrRef<Action::ResultE,
+                                          MultiSwitchPtr       ,
+                                          CNodePtr        ,
+                                          Action         *>(&MultiSwitch::draw));
+
+    IntersectAction::registerEnterDefault(
+        getClassType(),
+        osgTypedMethodFunctor2BaseCPtrRef<Action::ResultE,
+                                          MultiSwitchPtr       ,
+                                          CNodePtr        ,
+                                          Action         *>(&MultiSwitch::intersect));
+}
+
+
+/***************************************************************************\
+ *                           Instance methods                              *
+\***************************************************************************/
+
+/*-------------------------------------------------------------------------*\
+ -  private                                                                 -
+\*-------------------------------------------------------------------------*/
+
+/*----------------------- constructors & destructors ----------------------*/
+
+MultiSwitch::MultiSwitch(void) :
+    Inherited()
+{
+}
+
+MultiSwitch::MultiSwitch(const MultiSwitch &source) :
+    Inherited(source)
+{
+}
+
+MultiSwitch::~MultiSwitch(void)
+{
+}
+
+/*! Sets \c _mfChoices such that it only contains those indices that correspond
+    to ones in \a childMask, i.e. if \a childMask is \c 00101 (binary) children
+    with indices 0 and 2 are selected.
+ */
+void MultiSwitch::setBitfield(const BitVector childMask)
+{
+    _mfChoices.clear();
+    
+    BitVector anchor = 1;
+    for(UInt32 idx = 0; idx < 64; ++idx)
+    {
+        BitVector mask = anchor << idx;
+        
+        if(childMask & mask)
+            _mfChoices.push_back(idx);
+    }
+}
+
+/*-------------------------------------------------------------------------*/
+/*                               Draw                                      */
+
+Action::ResultE MultiSwitch::draw(Action *action)
+{
+    Action::ResultE  returnValue = Action::Continue;
+    DrawActionBase  *da          = dynamic_cast<DrawActionBase *>(action);
+
+    switch(getSwitchMode())
+    {
+    default:
+        FWARNING(("MultiSwitch::draw: Invalid value in _sfSwitchMode.\n"));
+        // fall through!
+    case CHOICES:
+    {
+        if(_mfChoices.empty())
+            returnValue = Action::Skip;
+
+        da->useNodeList();
+
+        //
+        // Assumption: _mfChoices is sorted; see function changed
+        //
+        MFUInt32::const_iterator iter = _mfChoices.begin();
+        MFUInt32::const_iterator end  = _mfChoices.end();
+
+        for(UInt32 idx = 0, num = action->getNNodes(); idx < num; ++idx)
+        {
+            if(idx == *iter)
+            {
+                if(da->isVisible(getCPtr(action->getNode(idx))))
+                    da->addNode(action->getNode(idx));
+
+                ++iter;
+
+                if(iter == end)
+                    break;
+            }
+        }
+    }
+    break;
+    case NONE:
+    {
+        returnValue = Action::Skip;
+    }
+    break;
+    case ALL:
+    {
+        if(da->selectVisibles() == 0)
+            returnValue = Action::Skip;
+    }
+    break;
+    }
+
+    return returnValue;
+}
+
+Action::ResultE MultiSwitch::intersect(Action *action)
+{
+    Action::ResultE   returnValue = Action::Continue;
+    IntersectAction  *da          = dynamic_cast<IntersectAction *>(action);
+
+    switch(getSwitchMode())
+    {
+    default:
+        FWARNING(("MultiSwitch::intersect: Invalid value in _sfSwitchMode.\n"));
+        // fall through!
+    case CHOICES:
+    {
+        if(_mfChoices.empty())
+            returnValue = Action::Skip;
+
+        //
+        // Assumption: _mfChoices is sorted; see function changed
+        //
+        MFUInt32::const_iterator iter = _mfChoices.begin();
+        MFUInt32::const_iterator end  = _mfChoices.end();
+
+        for(UInt32 idx = 0, num = action->getNNodes(); idx < num; ++idx)
+        {
+            if(idx == *iter)
+            {
+                da->addNode(action->getNode(idx));
+
+                ++iter;
+
+                if(iter == end)
+                    break;
+            }
+        }
+    }
+    break;
+    case NONE:
+    {
+        returnValue = Action::Skip;
+    }
+    break;
+    case ALL:
+    {
+        returnValue = Action::Continue;
+    }
+    break;
+    }
+
+    return returnValue;
+}
+/*----------------------------- class specific ----------------------------*/
+
+void MultiSwitch::changed(BitVector whichField, UInt32 origin)
+{
+    Inherited::changed(whichField, origin);
+    
+    if(whichField & ChoicesFieldMask)
+    {
+        // sort _mfChoices and remove duplicates
+        
+        std::sort(_mfChoices.begin(), _mfChoices.end());
+        
+        _mfChoices.resize(
+            std::distance(_mfChoices.begin(),
+                          std::unique(_mfChoices.begin(),
+                                      _mfChoices.end  () )));
+    }
+}
+
+void MultiSwitch::dump(      UInt32    , 
+                         const BitVector ) const
+{
+    SLOG << "Dump MultiSwitch NI" << std::endl;
+}
+
+
+/*------------------------------------------------------------------------*/
+/*                              cvs id's                                  */
+
+#ifdef OSG_SGI_CC
+#pragma set woff 1174
+#endif
+
+#ifdef OSG_LINUX_ICC
+#pragma warning( disable : 177 )
+#endif
+
+namespace
+{
+    static Char8 cvsid_cpp       [] = "@(#)$Id: OSGMultiSwitch.cpp,v 1.1 2008/08/19 15:25:07 neumannc Exp $";
+    static Char8 cvsid_hpp       [] = OSGMULTISWITCHBASE_HEADER_CVSID;
+    static Char8 cvsid_inl       [] = OSGMULTISWITCHBASE_INLINE_CVSID;
+
+    static Char8 cvsid_fields_hpp[] = OSGMULTISWITCHFIELDS_HEADER_CVSID;
+}
+
+#ifdef __sgi
+#pragma reset woff 1174
+#endif
+
+OSG_END_NAMESPACE
+
---
+++ Source/System/NodeCores/Groups/Misc/OSGMultiSwitch.h
@@ -0,0 +1,145 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *               Copyright (C) 2000-2002 by the OpenSG Forum                 *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+#ifndef _OSGMULTISWITCH_H_
+#define _OSGMULTISWITCH_H_
+#ifdef __sgi
+#pragma once
+#endif
+
+#include <OSGConfig.h>
+
+#include <OSGMultiSwitchBase.h>
+
+OSG_BEGIN_NAMESPACE
+
+/*! \brief MultiSwitch class. See \ref 
+           PageSystemMultiSwitch for a description.
+*/
+
+class OSG_SYSTEMLIB_DLLMAPPING MultiSwitch : public MultiSwitchBase
+{
+  private:
+
+    typedef MultiSwitchBase Inherited;
+
+    /*==========================  PUBLIC  =================================*/
+  public:
+    /*---------------------------------------------------------------------*/
+    /*! \name                       Enum                                   */
+    /*! \{                                                                 */
+
+    enum { ALL = -3, NONE = -2, CHOICES = -1 };
+    
+    void setBitfield(BitVector childMask);
+    
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                      Sync                                    */
+    /*! \{                                                                 */
+
+    virtual void changed(BitVector  whichField, 
+                         UInt32     origin    );
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                     Output                                   */
+    /*! \{                                                                 */
+
+    virtual void dump(      UInt32     uiIndent = 0, 
+                      const BitVector  bvFlags  = 0) const;
+
+    /*! \}                                                                 */
+    /*=========================  PROTECTED  ===============================*/
+  protected:
+
+    // Variables should all be in MultiSwitchBase.
+
+    /*---------------------------------------------------------------------*/
+    /*! \name                  Constructors                                */
+    /*! \{                                                                 */
+
+    MultiSwitch(void);
+    MultiSwitch(const MultiSwitch &source);
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                   Destructors                                */
+    /*! \{                                                                 */
+
+    virtual ~MultiSwitch(void); 
+
+    /*! \}                                                                 */
+    
+    /*---------------------------------------------------------------------*/
+    /*! \name                   Draw                                       */
+    /*! \{                                                                 */
+
+    Action::ResultE draw(Action *action);
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                 Intersect                                    */
+    /*! \{                                                                 */
+
+    Action::ResultE intersect(Action *action);
+
+    /*! \}                                                                 */  
+    /*==========================  PRIVATE  ================================*/
+  private:
+
+    friend class FieldContainer;
+    friend class MultiSwitchBase;
+
+    static void initMethod(void);
+
+    // prohibit default functions (move to 'public' if you need one)
+
+    void operator =(const MultiSwitch &source);
+};
+
+typedef MultiSwitch *MultiSwitchP;
+
+OSG_END_NAMESPACE
+
+#include <OSGMultiSwitchBase.inl>
+#include <OSGMultiSwitch.inl>
+
+#define OSGMULTISWITCH_HEADER_CVSID "@(#)$Id: OSGMultiSwitch.h,v 1.1 2008/08/19 15:25:08 neumannc Exp $"
+
+#endif /* _OSGMULTISWITCH_H_ */
